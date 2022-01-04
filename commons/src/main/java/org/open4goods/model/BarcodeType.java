b417dedd4fb725090d1ed4c181da763e85671bb7
GoulvenF <goulven.furet@gmail.com>
2022-01-04 19:45:00 +0100

    retro

--- commons/src/main/java/org/open4goods/model/BarcodeType.java
+++ commons/src/main/java/org/open4goods/model/BarcodeType.java
@@ -1,7 +1,9 @@
 package org.open4goods.model;
 
 public enum BarcodeType {
-	ISBN_13, GTIN_13, GTIN_8, GTIN_12, UNKNOWN
+	ISBN_13, GTIN_13, GTIN_8, GTIN_12, UNKNOWN,
+	// TODO delete
+	EAN_13
 	
 
 }
