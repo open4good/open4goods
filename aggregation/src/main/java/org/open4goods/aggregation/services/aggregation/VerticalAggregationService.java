9b45315284209927b3a55a2dd94ff89e1313e4fb
goulven <goulven@goulven-Aspire-VN7-791>
2023-05-18 17:24:07 +0200

    category

--- aggregation/src/main/java/org/open4goods/aggregation/services/aggregation/VerticalAggregationService.java
+++ aggregation/src/main/java/org/open4goods/aggregation/services/aggregation/VerticalAggregationService.java
@@ -1,5 +1,6 @@
 package org.open4goods.aggregation.services.aggregation;
 
+import org.apache.commons.lang3.StringUtils;
 import org.open4goods.aggregation.AbstractAggregationService;
 import org.open4goods.config.yml.ui.VerticalConfig;
 import org.open4goods.model.data.DataFragment;
@@ -24,16 +25,21 @@ public class VerticalAggregationService extends AbstractAggregationService {
 	@Override
 	public void onDataFragment(final DataFragment input, final AggregatedData output) {
 
-		VerticalConfig vertical = verticalService.getVerticalForCategoryName(input.getCategory());
-		
-		
-		if (null != vertical) {
-			if ( null != output.getVertical() && !vertical.getId().equals(output.getVertical())) {
-				dedicatedLogger.warn("Will erase existing vertical {} with {} for product {}, because of category {}", output.getVertical(), vertical.getId(), output.bestName(), input.getCategory());
-			} 			
-			output.setVertical(vertical.getId());
+		if (!StringUtils.isEmpty(input.getCategory())) {
 			
-		}		
+			// Adding datasource category
+			output.getDatasourceCategories().add(input.getCategory());
+	
+			// Adding vertical
+			VerticalConfig vertical = verticalService.getVerticalForCategoryName(input.getCategory());	
+			
+			if (null != vertical) {
+				if ( null != output.getVertical() && !vertical.getId().equals(output.getVertical())) {
+					dedicatedLogger.warn("Will erase existing vertical {} with {} for product {}, because of category {}", output.getVertical(), vertical.getId(), output.bestName(), input.getCategory());
+				} 			
+				output.setVertical(vertical.getId());
+				
+			}		
+		}
 	}
-
 }
