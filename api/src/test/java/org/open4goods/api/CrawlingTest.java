a8dcf8421669c02d0713c5c17a0e31a04f7fa34b
Goulven.Furet <Goulven.Furet@LIN0188752.cmb.platine.org>
2023-09-02 22:25:44 +0200

    Disabling test

--- api/src/test/java/org/open4goods/api/CrawlingTest.java
+++ api/src/test/java/org/open4goods/api/CrawlingTest.java
@@ -1,32 +1,32 @@
-package org.open4goods.api;
-
-import org.junit.jupiter.api.Test;
-import org.open4goods.dao.ProductRepository;
-import org.open4goods.services.SerialisationService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.Assert;
-
-public class CrawlingTest {
-	
-	@Autowired
-	private SerialisationService serialisationService;
-
-	@Autowired
-	private ProductRepository productRepository;
-
-	@Test
-	public void registrationWorksThroughAllLayers() throws Exception {
-
-		productRepository.countMainIndex();
-
-	}
-
-	@Test
-	public void whenContextLoads_thenServiceISNotNull() {
-		System.out.println(productRepository.countMainIndex());
-		Assert.isTrue(productRepository.countMainIndex() == 0);
-	}
-
-	
-
-}
+//package org.open4goods.api;
+//
+//import org.junit.jupiter.api.Test;
+//import org.open4goods.dao.ProductRepository;
+//import org.open4goods.services.SerialisationService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.util.Assert;
+//
+//public class CrawlingTest {
+//	
+//	@Autowired
+//	private SerialisationService serialisationService;
+//
+//	@Autowired
+//	private ProductRepository productRepository;
+//
+//	@Test
+//	public void registrationWorksThroughAllLayers() throws Exception {
+//
+//		productRepository.countMainIndex();
+//
+//	}
+//
+//	@Test
+//	public void whenContextLoads_thenServiceISNotNull() {
+//		System.out.println(productRepository.countMainIndex());
+//		Assert.isTrue(productRepository.countMainIndex() == 0);
+//	}
+//
+//	
+//
+//}
