35a825be78de0afaff07e7baf115c36d14b4a4e9
Goulven.Furet <Goulven.Furet@LIN0188752.cmb.platine.org>
2023-10-24 22:36:48 +0200

    misc

--- ui/src/main/java/org/open4goods/ui/helper/UiHelper.java
+++ ui/src/main/java/org/open4goods/ui/helper/UiHelper.java
@@ -1,5 +1,6 @@
 package org.open4goods.ui.helper;
 
+import org.open4goods.config.yml.attributes.AttributeConfig;
 import org.open4goods.config.yml.ui.VerticalConfig;
 import org.open4goods.services.VerticalsConfigService;
 
@@ -27,7 +28,12 @@ public class UiHelper {
 	 */
 	public String attributeName(String key) {
 		
-		return verticalConfig.getAttributesConfig().getAttributeConfigByKey(key).i18n(request.getLocale().getLanguage());
+		 AttributeConfig attr = verticalConfig.getAttributesConfig().getAttributeConfigByKey(key);
+		 
+		 if (null == attr) {
+			 return key +" (!)";
+		 }
+		return attr.i18n(request.getLocale().getLanguage());
 		
 		
 	}
