2103ab63470e5460d29bda8e5b932327e6c95f2e
goulven <goulven@goulven-Aspire-VN7-791>
2024-01-11 01:20:44 +0100

    loglevel

--- crawler/src/main/java/org/open4goods/crawler/services/fetching/CsvIndexationWorker.java
+++ crawler/src/main/java/org/open4goods/crawler/services/fetching/CsvIndexationWorker.java
@@ -438,7 +438,7 @@ public class CsvIndexationWorker implements Runnable {
 		if (!StringUtils.isEmpty(csvProperties.getAffiliatedUrl())) {
 			String u = getFromCsvRow(item, csvProperties.getAffiliatedUrl());
 			if (null == u) {
-				dedicatedLogger.warn("Null affiliated url in {}", item);	
+				dedicatedLogger.info("Null affiliated url in {}", item);	
 			} else {
 				if (null != csvProperties.getAffiliatedUrlReplacementTokens()) {
 					for (Entry<String, String> a : csvProperties.getAffiliatedUrlReplacementTokens().entrySet()) {
