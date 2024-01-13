c09f58ed8fc8099d4d9c3d347ae7b768767d6808
goulven <goulven@goulven-Aspire-VN7-791>
2024-01-13 06:50:15 +0100

    Using RedisRepository instead of redisTemplate

---
+++ commons/src/main/java/org/open4goods/store/repository/RedisProductRepository.java
@@ -0,0 +1,16 @@
+package org.open4goods.store.repository;
+
+import org.open4goods.model.product.Product;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * This "blank" repository is useless, but it is the model trhat allows allows "easy" schema recreation for verticals (see VerticalGeneration.createIndex)
+ * @author goulven
+ *
+ */
+
+public interface  RedisProductRepository extends ElasticsearchRepository<Product, String> {
+
+
+
+}
