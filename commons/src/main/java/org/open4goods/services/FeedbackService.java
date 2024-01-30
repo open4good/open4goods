04d820ecf29035229dab25878ec7aaf1e36ae995
goulven <goulven@goulven-Aspire-VN7-791>
2024-01-30 12:13:15 +0100

    Feedback is up and running !

--- commons/src/main/java/org/open4goods/services/FeedbackService.java
+++ commons/src/main/java/org/open4goods/services/FeedbackService.java
@@ -3,14 +3,15 @@ package org.open4goods.services;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Set;
 
-import org.kohsuke.github.GHEventPayload.Issue;
+import org.apache.commons.lang3.StringUtils;
 import org.kohsuke.github.GHIssue;
 import org.kohsuke.github.GHIssueBuilder;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHubBuilder;
 import org.open4goods.config.yml.FeedbackConfiguration;
-import org.open4goods.config.yml.GithubConfiguration;
+import org.open4goods.exceptions.InvalidParameterException;
 import org.open4goods.helper.IdHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,34 +39,125 @@ public class FeedbackService {
 	}
 
 
-	
-	
-	
-	public GHIssue createIssue (String title, String description, String urlSource, String publisherName, Collection<String> labels) throws IOException {
-		
-		GHIssueBuilder issue = github.createIssue(IdHelper.sanitize(title));
+	/**
+	 * Get the template corresponding to an issue
+	 * @param title
+	 * @param description
+	 * @param urlSource
+	 * @param publisherName
+	 * @param labels
+	 * @return
+	 */
+	public String getIssueTemplate (String title, String description, String urlSource, String publisherName, Collection<String> labels) {
 		
 		StringBuilder messageMarkdown = new StringBuilder();
-		messageMarkdown.append("## Feedback\n\n");
-		messageMarkdown.append("### URL : " ).append(urlSource).append("\n\n");
-		messageMarkdown.append("### Author : " ).append(publisherName).append("\n\n");
-		messageMarkdown.append("### Message : \n" ).append(description).append("\n\n");
+		/**
+		## Issue
+		This feedback has been submited by *AUTHOR* on [nudger.fr](https://nudger.fr/), in order to declare an issue on [this page](PAGE).
+		#### User message : 
+		> MESSAGE
+		**/
+		messageMarkdown.append("## Bug ! \n\n");
+		messageMarkdown.append("This issue has been submited by *").append(publisherName).append("* on [nudger.fr](").append(urlSource).append("), in order to declare a bug");
 		
+		if (!StringUtils.isEmpty(urlSource)) {
+			messageMarkdown.append("\n\nThis bug is relative to [this page](").append(urlSource).append(").\n\n");
+		}
 		
+		messageMarkdown.append("\n\n");
+		messageMarkdown.append("#### User message : \n\n");
+		messageMarkdown.append("> ").append(description).append("\n\n");
 		
+		return messageMarkdown.toString();
+		}
+	
+	
+	/**
+	 * Get the template corresponding to an idea
+	 * @param title
+	 * @param description
+	 * @param urlSource
+	 * @param publisherName
+	 * @param labels
+	 * @return
+	 */
+	public String getIdeaTemplate (String title, String description, String urlSource, String publisherName, Collection<String> labels) {
 		
+		StringBuilder messageMarkdown = new StringBuilder();
+		/**
+		## Issue
+		This feedback has been submited by *AUTHOR* on [nudger.fr](https://nudger.fr/), in order to declare an issue on [this page](PAGE).
+		#### User message : 
+		> MESSAGE
+		**/
+		messageMarkdown.append("## Idea \n\n");
+		messageMarkdown.append("This feedback has been submited by *").append(publisherName).append("* on [nudger.fr](").append(urlSource).append("), in order to declare a brillant idea.");
 		
-		// TODO : Templatize the description
-		issue = issue.body(messageMarkdown.toString());
+		if (!urlSource.isEmpty()) {
+			messageMarkdown.append("\n\nThis idea could be related to [this page](").append(urlSource).append(").\n\n");
+		}
+		
+		messageMarkdown.append("\n\n");
+		messageMarkdown.append("#### User message : \n\n");
+		messageMarkdown.append("> ").append(description).append("\n\n");
+		
+		return messageMarkdown.toString();
+		}
+	
+	/**
+	 * Create an issue on github
+	 * @param title
+	 * @param description
+	 * @param labels
+	 * @return
+	 * @throws IOException
+	 * @throws InvalidParameterException
+	 */
+	
+	public GHIssue createIssue (String title, String description, Collection<String> labels) throws IOException, InvalidParameterException {
+		
+		GHIssueBuilder issue = github.createIssue(IdHelper.sanitize(title));
+
+		issue = issue.body(description);
 		
 		for (String label : labels) {
 			issue = issue.label(label);
 		}
 		
-		GHIssue ret = issue.create();
-		
+		GHIssue ret = issue.create();		
 		return ret;
 	
 	}
+
+	/**
+	 * Create a bug issue on github
+	 * @param title
+	 * @param description
+	 * @param url
+	 * @param author
+	 * @param labels
+	 * @throws IOException
+	 * @throws InvalidParameterException
+	 */
+	public void createBug(String title, String description, String url, String author, Set<String> labels) throws IOException, InvalidParameterException {
+		String message = getIssueTemplate(title, description, url, author, labels);
+		createIssue(title, message, labels);
+	}
 	
+	
+	/**
+	 * Create an ideaissue on github
+	 * @param title
+	 * @param description
+	 * @param url
+	 * @param author
+	 * @param labels
+	 * @throws IOException
+	 * @throws InvalidParameterException
+	 */
+	public void createIdea(String title, String description, String url, String author, Set<String> labels) throws IOException, InvalidParameterException {
+		String message = getIssueTemplate(title, description, url, author, labels);
+		createIssue(title, message, labels);
+
+	}
 }
--- ui/src/main/java/org/open4goods/ui/controllers/ui/FeedbackController.java
+++ ui/src/main/java/org/open4goods/ui/controllers/ui/FeedbackController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.open4goods.exceptions.InvalidParameterException;
 import org.open4goods.services.FeedbackService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,65 +29,80 @@ public class FeedbackController extends AbstractUiController {
 		this.feedbackService = feedbackService;
 	}
 
-//	public ModelAndView issue(final HttpServletRequest request,  @RequestParam("message") String message, @RequestParam("url") String urlSource, @RequestParam("author") String author, @RequestParam("author") String captcha) {
-
 	@GetMapping("/feedback/issue")
 	public ModelAndView issue(final HttpServletRequest request,  @RequestParam(required = false, name = "url") String url) {
-
-		
-//		if (captcha == null || !captcha.equals("1")) {
-//			LOGGER.warn("Captcha is not valid");
-//			return defaultModelAndView("feedback-error", request);
-//		}
-//		
-		
-		
 		
 		ModelAndView model = defaultModelAndView("feedback-issue", request);
+
+		// Adding authenticated user
+		if (null != model.getModel().get("user")) {
+			model.addObject("author", model.getModel().get("user"));
+		}
 		model.addObject("urlSource",url);
 		
 		return model;
 	}
 
-	@PostMapping("/feedback/issue")
+	@GetMapping("/feedback/idea")
+	public ModelAndView idea(final HttpServletRequest request,  @RequestParam(required = false, name = "url") String url) {
+		
+		ModelAndView model = defaultModelAndView("feedback-idea", request);
+
+		// Adding authenticated user
+		if (null != model.getModel().get("user")) {
+			model.addObject("author", model.getModel().get("user"));
+		}
+		model.addObject("urlSource",url);
+		
+		return model;
+	}
+	
+	
+	@PostMapping("/feedback")
 	public ModelAndView createIssue(final HttpServletRequest request, @RequestBody MultiValueMap<String, String> formData) {
 
 		if (formData == null) {
 			LOGGER.warn("No form data");
-			return defaultModelAndView("feedback-error", request);
+			
+			return defaultModelAndView("feedback-error", request).addObject("msg","No form data");
 		}
 		
 		if (formData.getFirst("captcha") == null || !formData.getFirst("captcha").equals("1")) {
 			LOGGER.warn("Captcha is not valid");
-			return defaultModelAndView("feedback-error", request);
+			return defaultModelAndView("feedback-error", request).addObject("msg","Invalid captcha");
 		}
 
 
 		try {
 			Set<String> labels = new HashSet<String>();
+			// TODO : from conf
+			labels.add("nudger.fr");
 			labels.add("feedback");
-			labels.add("issue");
+
+			if (formData.getFirst("type").equals("bug")) {
+				labels.add("bug");
+				
+			} else if (formData.getFirst("type").equals("idea")) {
+				labels.add("feature");
+			} 
 			
-			feedbackService.createIssue(formData.getFirst("title"),formData.getFirst("message"), formData.getFirst("url"), formData.getFirst("author"), labels);
+			String url = formData.getFirst("url");
+			if (url.equals("/")) {
+				url = config.getBaseUrl(request.getLocale());
+			}
+			
+			feedbackService.createBug(formData.getFirst("title"),formData.getFirst("message"), url, formData.getFirst("author"), labels);
 			
 		
 		} catch (IOException e) {
 			LOGGER.error("Error while creating issue", e);
-			return defaultModelAndView("feedback-error", request);
+			return defaultModelAndView("feedback-error", request).addObject("msg","Internal exception : "+e.getMessage());
+		} catch (InvalidParameterException e) {
+			LOGGER.error("Error while creating issue, invalid parameters", e);
+			return defaultModelAndView("feedback-error", request).addObject("msg","Invalid parameter exception : "+e.getMessage());
 		}
 		
-		
-		
-		
-		return defaultModelAndView("feedback-success", request);
-	}
-	
-	@GetMapping("/feedback/idea")
-	public ModelAndView idea(final HttpServletRequest request) {
-
-		ModelAndView model = defaultModelAndView("feedback-idea", request);
-		
-		return model;
+		return defaultModelAndView("feedback-success", request).addObject("backUrl",formData.getFirst("url"));
 	}
 
 
