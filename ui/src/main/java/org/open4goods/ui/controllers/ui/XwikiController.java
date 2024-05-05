85fb30cccf6527a5a5f269516cd1a7e295016678
goulven <goulven.furet@gmail.com>
2024-05-05 21:57:59 +0200

    xwiki-spring-boot-starter integration

--- ui/src/main/java/org/open4goods/ui/controllers/ui/XwikiController.java
+++ ui/src/main/java/org/open4goods/ui/controllers/ui/XwikiController.java
@@ -1,32 +1,11 @@
 package org.open4goods.ui.controllers.ui;
 
-import java.io.IOException;
-
-import org.apache.commons.lang3.StringUtils;
-import org.open4goods.config.yml.ui.VerticalConfig;
-import org.open4goods.exceptions.InvalidParameterException;
-import org.open4goods.exceptions.ResourceNotFoundException;
-import org.open4goods.exceptions.TechnicalException;
-import org.open4goods.model.constants.RolesConstants;
-import org.open4goods.model.dto.WikiResult;
-import org.open4goods.services.VerticalsConfigService;
-import org.open4goods.services.XwikiService;
 import org.open4goods.ui.config.yml.UiConfig;
-import org.open4goods.xwiki.services.XwikiMappingService;
 import org.open4goods.xwiki.services.XWikiReadService;
+import org.open4goods.xwiki.services.XwikiMappingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.ModelAndView;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 
@@ -44,19 +23,12 @@ public class XwikiController extends AbstractUiController {
 	private final UiConfig config;
 
 	private final XWikiReadService xwikiService;
-
-	// TODO : Tweak pageSize handle categories search page
-	private final VerticalController verticalController;
-	private final VerticalsConfigService verticalService;
-
 	private XwikiMappingService mappingService;
 
-	public XwikiController(UiConfig config, XwikiMappingService mappingService, XWikiReadService xwikiService, VerticalController verticalController, VerticalsConfigService verticalsConfigService) {
+	public XwikiController(UiConfig config, XwikiMappingService mappingService, XWikiReadService xwikiService) {
 		this.config = config;
 		this.xwikiService = xwikiService;
 		this.mappingService = mappingService;
-		this.verticalController = verticalController;
-		this.verticalService = verticalsConfigService;
 	}
 
 
@@ -89,107 +61,107 @@ public class XwikiController extends AbstractUiController {
 //		return mv;
 //
 //	}
+//
+//	@GetMapping("/{page:[a-z-]+}")
+//	//TODO : Ugly url's mappings
+//	public ModelAndView xwiki(
+//			@PathVariable(name = "page") String page, final HttpServletRequest request, HttpServletResponse response)
+//					throws IOException, TechnicalException, InvalidParameterException {
+//
+//		
+//		// Testing if a custom page in this vertical		
+//		String tpl = config.getPages().get(page);
+//		if (null != tpl) {
+//			ModelAndView mv = defaultModelAndView(tpl, request);
+//			return mv;
+//		}
+//		
+//		
+//		// TODO : Twweak for verticalsLanguagesByUrl
+//
+//
+//		if (null != verticalService.getVerticalForPath(page)) {
+//			return verticalController.home(page,request);
+//		} else
+//			return xwiki("Main", page, request,response );
+//	}
 
-	@GetMapping("/{page:[a-z-]+}")
-	//TODO : Ugly url's mappings
-	public ModelAndView xwiki(
-			@PathVariable(name = "page") String page, final HttpServletRequest request, HttpServletResponse response)
-					throws IOException, TechnicalException, InvalidParameterException {
 
-		
-		// Testing if a custom page in this vertical		
-		String tpl = config.getPages().get(page);
-		if (null != tpl) {
-			ModelAndView mv = defaultModelAndView(tpl, request);
-			return mv;
-		}
-		
-		
-		// TODO : Twweak for verticalsLanguagesByUrl
+//	@GetMapping("/attachments/{space}/{page}/{filename}")
+//	
+//	// TODO : Caching
+//	public void xwiki(@PathVariable(name = "space") String space, @PathVariable(name = "page") String page,
+//            @PathVariable(name = "filename") String filename, final HttpServletRequest request,
+//            HttpServletResponse response) throws IOException, TechnicalException, InvalidParameterException {
+//
+//        // TODO : Twweak for verticalsLanguagesByUrl
+//
+//        String url = mappingService. getAttachmentUrl(space, page, filename);
+//        
+//        // TODO : ugly, should fetch the meta (mime type is availlable in xwiki service), but does not work for the blog image, special class and not appears in attachments list
+//		if (url.endsWith(".pdf")) {
+//			response.setContentType("application/pdf");
+//		} else if (url.endsWith(".jpg") || url.endsWith(".jpeg")) {
+//			response.setContentType("image/jpeg");
+//		} else if (url.endsWith(".png")) {
+//			response.setContentType("image/png");
+//		} else if (url.endsWith(".gif")) {
+//			response.setContentType("image/gif");
+//		}
+//        
+//        response.getOutputStream().write(xwikiAttachmentService.downloadAttachment(url));
+//                
+//	}
+//			
 
-
-		if (null != verticalService.getVerticalForPath(page)) {
-			return verticalController.home(page,request);
-		} else
-			return xwiki("Main", page, request,response );
-	}
-
-
-	@GetMapping("/attachments/{space}/{page}/{filename}")
-	
-	// TODO : Caching
-	public void xwiki(@PathVariable(name = "space") String space, @PathVariable(name = "page") String page,
-            @PathVariable(name = "filename") String filename, final HttpServletRequest request,
-            HttpServletResponse response) throws IOException, TechnicalException, InvalidParameterException {
-
-        // TODO : Twweak for verticalsLanguagesByUrl
-
-        String url = mappingService. getAttachmentUrl(space, page, filename);
-        
-        // TODO : ugly, should fetch the meta (mime type is availlable in xwiki service), but does not work for the blog image, special class and not appears in attachments list
-		if (url.endsWith(".pdf")) {
-			response.setContentType("application/pdf");
-		} else if (url.endsWith(".jpg") || url.endsWith(".jpeg")) {
-			response.setContentType("image/jpeg");
-		} else if (url.endsWith(".png")) {
-			response.setContentType("image/png");
-		} else if (url.endsWith(".gif")) {
-			response.setContentType("image/gif");
-		}
-        
-        response.getOutputStream().write(xwikiAttachmentService.downloadAttachment(url));
-                
-	}
-			
-
-	@GetMapping("/{vertical:[a-z-]+}/{page:[a-z-]+}")
-	public ModelAndView xwiki(@PathVariable(name = "vertical") String vertical,
-			@PathVariable(name = "page") String page, final HttpServletRequest request, HttpServletResponse response) throws IOException
-					 {
-
-		ModelAndView mv = null;
-
-		
-		// Testing if a custom page in this vertical		
-		VerticalConfig vc = verticalService.getVerticalForPath(vertical);
-		String lang = verticalService.getLanguageForPath(vertical);
-		
-		
-		if (null != vc) {
-			String tpl = vc.i18n(lang). getPages().get(page);
-			if (null != tpl) {
-				mv = defaultModelAndView(tpl, request);
-				return mv;
-			}
-		}
-		
-		
-		if (null != request.getParameter("edit")) {
-			// Edit mode, redirect pageSize the wiki
-
-			response.sendRedirect(config.getWikiConfig().getEditUrl(vertical,page));
-		} else {
-			// Rendering mode
-
-			WikiResult content = null;
-			try {
-				content = xwikiService.getPage(vertical + "/" + page);
-			} catch (TechnicalException e) {
-				throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal error");	
-			} catch (ResourceNotFoundException e) {
-				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content Not Found");	
-			}
-
-			if (StringUtils.isEmpty(content.getHtml())) {
-				//				mv.setStatus(HttpStatus.NOT_FOUND);
-				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content Not Found");		    
-			} else {
-				mv = defaultModelAndView(("xwiki-"+content.getLayout()), request);
-				mv.addObject("content", content);
-			}
-		}
-
-		return mv;
-	}
+//	@GetMapping("/{vertical:[a-z-]+}/{page:[a-z-]+}")
+//	public ModelAndView xwiki(@PathVariable(name = "vertical") String vertical,
+//			@PathVariable(name = "page") String page, final HttpServletRequest request, HttpServletResponse response) throws IOException
+//					 {
+//
+//		ModelAndView mv = null;
+//
+//		
+//		// Testing if a custom page in this vertical		
+//		VerticalConfig vc = verticalService.getVerticalForPath(vertical);
+//		String lang = verticalService.getLanguageForPath(vertical);
+//		
+//		
+//		if (null != vc) {
+//			String tpl = vc.i18n(lang). getPages().get(page);
+//			if (null != tpl) {
+//				mv = defaultModelAndView(tpl, request);
+//				return mv;
+//			}
+//		}
+//		
+//		
+//		if (null != request.getParameter("edit")) {
+//			// Edit mode, redirect pageSize the wiki
+//
+//			response.sendRedirect(config.getWikiConfig().getEditUrl(vertical,page));
+//		} else {
+//			// Rendering mode
+//
+//			WikiResult content = null;
+//			try {
+//				content = xwikiService.getPage(vertical + "/" + page);
+//			} catch (TechnicalException e) {
+//				throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal error");	
+//			} catch (ResourceNotFoundException e) {
+//				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content Not Found");	
+//			}
+//
+//			if (StringUtils.isEmpty(content.getHtml())) {
+//				//				mv.setStatus(HttpStatus.NOT_FOUND);
+//				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content Not Found");		    
+//			} else {
+//				mv = defaultModelAndView(("xwiki-"+content.getLayout()), request);
+//				mv.addObject("content", content);
+//			}
+//		}
+//
+//		return mv;
+//	}
 
 }
