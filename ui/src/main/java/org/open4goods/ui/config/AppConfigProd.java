367e780439f8413ee797a1e1bbd8264fb34b7355
Goulven.Furet <Goulven.Furet@LIN0188752.cmb.platine.org>
2024-05-26 21:52:33 +0200

    Fixing sitemap path

--- ui/src/main/java/org/open4goods/ui/config/AppConfigProd.java
+++ ui/src/main/java/org/open4goods/ui/config/AppConfigProd.java
@@ -1,7 +1,9 @@
 package org.open4goods.ui.config;
 
+import java.io.File;
 import java.util.List;
 
+import org.open4goods.ui.config.yml.UiConfig;
 import org.open4goods.ui.interceptors.GenericTemplateInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.UrlPathHelper;
 
@@ -16,6 +19,12 @@ import org.springframework.web.util.UrlPathHelper;
 @Profile({"beta","prod"})
 public class AppConfigProd {
 
+	
+	private final UiConfig config;
+	
+	public AppConfigProd(UiConfig config) {
+		this.config = config;
+	}
 
 	///////////////////////////////////
 	// Resources
@@ -40,6 +49,20 @@ public class AppConfigProd {
 				UrlPathHelper urlPathHelper = new UrlPathHelper();
 				urlPathHelper.setUrlDecode(false);
 				configurer.setUrlPathHelper(urlPathHelper);
+			}
+			
+			@Override
+			public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+
+//				registry.setOrder(Ordered.LOWEST_PRECEDENCE);
+				registry.addResourceHandler("/sitemap/**").addResourceLocations("file:" + config.siteMapFolder().getAbsolutePath() + File.separator);
+
+//				// NOTE : Register here files / folder to be allowed in devmode
+//				registerFallbackFolder(registry,"assets");
+//				registerFallbackFolder(registry,"css");
+//				registerFallbackFolder(registry,"icons");
+//				registerFallbackFolder(registry,"vendor");
+//				registerFile(registry,"tpl_table.html");
 			}
 		};
 	}
