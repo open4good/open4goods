01525912facab95d08eb8f6a12cc5019aea5247f
Goulven.Furet <Goulven.Furet@LIN0188752.cmb.platine.org>
2024-05-28 23:46:11 +0200

    first approach

---
+++ commons/src/main/java/org/open4goods/config/yml/IcecatFeatureConfiguration.java
@@ -0,0 +1,17 @@
+package org.open4goods.config.yml;
+
+public class IcecatFeatureConfiguration {
+
+	private String featuresListFileUri= "/home/Goulven.Furet/Bureau/icecat/FeaturesList.xml";
+
+	public String getFeaturesListFileUri() {
+		return featuresListFileUri;
+	}
+
+	public void setFeaturesListFileUri(String featuresListFileUri) {
+		this.featuresListFileUri = featuresListFileUri;
+	}
+	
+	
+	
+}
