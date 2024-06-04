804d144d12903a4eba874f18dae4fe0f9cbf178c
scezen <nootybear@gmail.com>
2024-06-04 16:46:09 +0200

    Ajouts fonctionnalités principales

---
+++ commons/src/main/java/org/open4goods/config/yml/ui/ImageGenerationConfig.java
@@ -0,0 +1,14 @@
+package org.open4goods.config.yml.ui;
+
+public class ImageGenerationConfig {
+
+    private String prompt;
+
+    public String getPrompt() {
+        return prompt;
+    }
+
+    public void setPrompt(String prompt) {
+        this.prompt = prompt;
+    }
+}
