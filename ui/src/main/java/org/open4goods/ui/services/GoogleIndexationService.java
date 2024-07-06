b21f96e459ae0645f0adabccefccc82c45c10efe
Goulven.Furet <Goulven.Furet@github.org>
2024-07-06 20:25:03 +0200

    Enabling google indexation

--- ui/src/main/java/org/open4goods/ui/services/GoogleIndexationService.java
+++ ui/src/main/java/org/open4goods/ui/services/GoogleIndexationService.java
@@ -14,6 +14,7 @@ import org.open4goods.dao.ProductRepository;
 import org.open4goods.services.VerticalsConfigService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.batch.BatchRequest;
