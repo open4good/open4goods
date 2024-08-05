f11a39dda4bdbc74e34f5d43640522efeb6ece9b
goulven <goulven@goulven-Ionico-IV-15>
2024-08-05 08:54:22 +0200

    Spring boot admin server

---
+++ admin/src/main/java/org/open4goods/admin/AdminApplication.java
@@ -0,0 +1,16 @@
+package org.open4goods.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableAdminServer
+@SpringBootApplication
+public class AdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApplication.class, args);
+	}
+
+}
