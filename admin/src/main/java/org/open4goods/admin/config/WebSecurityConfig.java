df35234f1c7009c4fe6f7d80091c56ae66b3572b
goulven <goulven@goulven-Ionico-IV-15>
2024-08-05 09:17:38 +0200

    Layouts / renaming

--- admin/src/main/java/org/open4goods/admin/config/WebSecurityConfig.java
+++ admin/src/main/java/org/open4goods/admin/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package org.open4goods.admin;
+package org.open4goods.admin.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
