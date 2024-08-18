4ed9d2cb93d05d1a12625a42e0cdcc7c40b26bf5
goulven <goulven@goulven-Ionico-IV-15>
2024-08-18 22:19:53 +0200

    Config and HEalthcheck

--- api/src/main/java/org/open4goods/api/config/yml/BackupConfig.java
+++ api/src/main/java/org/open4goods/api/config/yml/BackupConfig.java
@@ -25,6 +25,43 @@ public class BackupConfig {
 	@NotEmpty
 	private String importProductPath;
 	
+
+	/**
+	 * Size of the bulk to use for products importation
+	 */
+	private  int importBulkSize = 200;
+
+	/**
+	 * Number of threads (and of files) to operate export with
+	 */
+	private int productsExportThreads = 4;
+
+	
+	/**
+	 * Min size in MB the xwiki backup file should have (Will Raise an healthcheck.down() if this criteria is not met)
+	 */
+	private int minXwikiBackupFileSizeInMb = 30;
+	
+	/**
+	 * Min size in MB the product backup folder should have (Will Raise an healthcheck.down() if this criteria is not met)
+	 */
+	
+	private int minProductsBackupFolderSizeInMb = 20000;
+
+
+	/**
+	 * Max age the wiki must have, in hours. (Will Raise an healthcheck.down() if this criteria is not met)
+	 */
+	private int maxWikiBackupAgeInHours = 14;
+	
+
+	/**
+	 * Max age the products file must have, in hours. (Will Raise an healthcheck.down() if this criteria is not met)
+	 */
+	private long maxProductsBackupAgeInHours = 1000 * 3600 * 24 * 8;
+	
+	
+	
 	
 	public String getXwikiBackupFile() {
 		return xwikiBackupFile;
@@ -48,6 +85,54 @@ public class BackupConfig {
 
 	public void setImportProductPath(String importProductPath) {
 		this.importProductPath = importProductPath;
+	}
+
+	public int getImportBulkSize() {
+		return importBulkSize;
+	}
+
+	public void setImportBulkSize(int importBulkSize) {
+		this.importBulkSize = importBulkSize;
+	}
+
+	public int getProductsExportThreads() {
+		return productsExportThreads;
+	}
+
+	public void setProductsExportThreads(int productsExportThreads) {
+		this.productsExportThreads = productsExportThreads;
+	}
+
+	public int getMinXwikiBackupFileSizeInMb() {
+		return minXwikiBackupFileSizeInMb;
+	}
+
+	public void setMinXwikiBackupFileSizeInMb(int minXwikiBackupFileSizeInMb) {
+		this.minXwikiBackupFileSizeInMb = minXwikiBackupFileSizeInMb;
+	}
+
+	public int getMinProductsBackupFolderSizeInMb() {
+		return minProductsBackupFolderSizeInMb;
+	}
+
+	public void setMinProductsBackupFolderSizeInMb(int minProductsBackupFolderSizeInMb) {
+		this.minProductsBackupFolderSizeInMb = minProductsBackupFolderSizeInMb;
+	}
+
+	public int getMaxWikiBackupAgeInHours() {
+		return maxWikiBackupAgeInHours;
+	}
+
+	public void setMaxWikiBackupAgeInHours(int maxWikiBackupAgeInHours) {
+		this.maxWikiBackupAgeInHours = maxWikiBackupAgeInHours;
+	}
+
+	public long getMaxProductsBackupAgeInHours() {
+		return maxProductsBackupAgeInHours;
+	}
+
+	public void setMaxProductsBackupAgeInHours(long maxProductsBackupAgeInHours) {
+		this.maxProductsBackupAgeInHours = maxProductsBackupAgeInHours;
 	}
 	
 	
