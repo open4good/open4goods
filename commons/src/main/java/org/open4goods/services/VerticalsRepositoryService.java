762710ebedb2f64f9ae4a4586405b341668a8f78
goulven <goulven@goulven-Ionico-IV-15>
2024-08-20 15:06:26 +0200

    Verticals specific indexes preparation

--- commons/src/main/java/org/open4goods/services/VerticalsRepositoryService.java
+++ commons/src/main/java/org/open4goods/services/VerticalsRepositoryService.java
@@ -2,120 +2,204 @@ package org.open4goods.services;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.open4goods.config.yml.ui.VerticalConfig;
+import org.open4goods.model.data.FeatureGroup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.IndexOperations;
 import org.springframework.data.elasticsearch.core.document.Document;
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-import jakarta.annotation.PostConstruct;
-
 /**
- * Service responsible for dynamic creation of specific vertical indexes.
- * This service handles the index creation, settings, and mappings for different verticals.
+ * Service responsible for dynamic creation of specific vertical indexes. This
+ * service handles the index creation, settings, and mappings for different
+ * verticals.
  * 
- * Improvements:
- * - Detailed comments added for better understanding.
- * - Added proper logging for better traceability.
- * - Improved method signature and parameter validation.
- * - Enhanced index creation flow with necessary checks.
+ * Improvements: - Detailed comments added for better understanding. - Added
+ * proper logging for better traceability. - Improved method signature and
+ * parameter validation. - Enhanced index creation flow with necessary checks.
  */
 @Service
 public class VerticalsRepositoryService {
 
 	private static final Logger logger = LoggerFactory.getLogger(VerticalsRepositoryService.class);
 
-	// Create an ObjectMapper used to build json progrmaticaly	
-    ObjectMapper mapper = new ObjectMapper();
-    
-    // Used to adress store R/W 
-    private ElasticsearchTemplate elasticsearchRestTemplate;
-	// Used to derivate schema from Icecat FeatureGroupsTaxonomy 
-    private IcecatService icecatService;
-    // Used for verticals configuration options
+	// Create an ObjectMapper used to build json progrmaticaly
+	ObjectMapper mapper = new ObjectMapper();
+
+	// Used to adress store R/W
+	private ElasticsearchTemplate elasticsearchRestTemplate;
+	// Used to derivate schema from Icecat FeatureGroupsTaxonomy
+	private IcecatService icecatService;
+	// Used for verticals configuration options
 	private VerticalsConfigService verticalsConfigService;
-    
-    /**
-     * Constructor for VerticalsRepositoryService.
-     *
-     * @param elasticOperations Elasticsearch operations instance for interacting with Elasticsearch.
-     * @param elasticsearchTemplate 
-     */
-    public VerticalsRepositoryService(ElasticsearchTemplate elasticsearchTemplate, VerticalsConfigService verticalsConfigService, IcecatService icecatService) {
-        this.elasticsearchRestTemplate = elasticsearchTemplate;
-        this.icecatService = icecatService;
-        this.verticalsConfigService = verticalsConfigService;
-    }
-    
 
-	@PostConstruct
-    public void initIndexes() {
-    	
-    }
-    
-    public void createIndex(VerticalConfig vConfig) throws IOException {
-    	
-    	String indexName = vConfig.indexName();
-    	IndexOperations indexOperations = elasticsearchRestTemplate.indexOps(IndexCoordinates.of(vConfig.indexName()));
+	/**
+	 * Constructor for VerticalsRepositoryService.
+	 *
+	 * @param elasticOperations     Elasticsearch operations instance for
+	 *                              interacting with Elasticsearch.
+	 * @param elasticsearchTemplate
+	 */
+	public VerticalsRepositoryService(ElasticsearchTemplate elasticsearchTemplate, VerticalsConfigService verticalsConfigService, IcecatService icecatService) {
+		this.elasticsearchRestTemplate = elasticsearchTemplate;
+		this.icecatService = icecatService;
+		this.verticalsConfigService = verticalsConfigService;
+		
+		// Indexes initialization
+		initIndexes();
+	}
 
-    	// Checking index exists
-    	if (indexOperations.exists()) {
-    		logger.info("Index already exists, skipped : {} ",indexName);
-    	} else {
-    		
-	        // Create the JSON structure programmatically
-	        ObjectNode rootNode = mapper.createObjectNode();
-	        ObjectNode propertiesNode = mapper.createObjectNode();
-	        
-	        configureAnalyser(rootNode);
-	        
-	        ObjectNode field1Node = mapper.createObjectNode();
-	        field1Node.put("type", "text");
-	        field1Node.put("analyzer", "standard");
-	
-	        ObjectNode field2Node = mapper.createObjectNode();
-	        field2Node.put("type", "keyword");
-	
-	        propertiesNode.set("field1", field1Node);
-	        propertiesNode.set("field2", field2Node);
-	
-	        rootNode.set("properties", propertiesNode);
-	
-	        // Convert ObjectNode to JSON string
-	        String jsonString = mapper.writeValueAsString(rootNode);
-	        
-	        
-	        
-	        String mappingJson = jsonString; //Pass json string here
-	        Document mapping = Document.parse(mappingJson);
-	        
-	        
-	        // Configure settings for the index (can be customized)
-	        Map<String, Object> settings = new HashMap<>();
-	        settings.put("index.number_of_shards", 1);
-	        settings.put("index.number_of_replicas",1);
-	        
-	
-	        indexOperations.create(settings, mapping);
-	        indexOperations.refresh(); //(Optional) refreshes the doc count
-    	}
-    }
+	public void initIndexes() {
+		for (VerticalConfig vConf : verticalsConfigService.getConfigsWithoutDefault()) {
+			try {
+				createIndex(vConf);
+			} catch (IOException e) {
+				// TODO Auto-generated catch blockvConf
+				e.printStackTrace();
+			}
+		}
+	}
 
-    /**
-     * Create the analyser part
-     * 
-     * {
+	public void createIndex(VerticalConfig vConfig) throws IOException {
 
+		
+		Map<String, String> features = icecatService.types(vConfig);
+		
+		// TODO : Clean the name
+		String indexName = vConfig.indexName();
+		IndexOperations indexOperations = elasticsearchRestTemplate.indexOps(IndexCoordinates.of(vConfig.indexName()));
+		
+		// TODO : Find the correspondinf attributes coverage
+		
+		
+		// Checking index exists
+		if (indexOperations.exists()) {
+			logger.info("Index already exists, skipped : {} ", indexName);
+		} else {
+
+			logger.info("Index does not exists, will be created : {} ", indexName);
+			
+			// Create the JSON structure programmatically
+			ObjectNode rootNode = mapper.createObjectNode();
+
+			// Add the analysers configuration
+			configureAnalyser(rootNode);
+
+			///////////////////////////////////////
+			// Add the mapping properties
+			///////////////////////////////////////
+			ObjectNode propertiesNode = mapper.createObjectNode();
+
+			// Add the generic fields
+
+			// TODO : Type and names as const
+			addFieldMapping(propertiesNode, "offersCount", "integer", false);
+			
+			// TODO : Fix analyser
+//			addFieldMapping(propertiesNode, "name", "text", "insensitiv", true, false);
+			addFieldMapping(propertiesNode, "name", "text" , true);
+
+			rootNode.set("properties", propertiesNode);
+
+			// Convert ObjectNode to JSON string
+			String jsonString = mapper.writeValueAsString(rootNode);
+
+			String mappingJson = jsonString; // Pass json string here
+			Document mapping = Document.parse(mappingJson);
+
+			// Configure settings for the index (can be customized)
+			Map<String, Object> settings = new HashMap<>();
+			settings.put("index.number_of_shards", 1);
+			settings.put("index.number_of_replicas", 1);
+
+			indexOperations.create(settings, mapping);
+			indexOperations.refresh(); // (Optional) refreshes the doc count
+		}
+	}
+
+	/**
+	 * Add a field to the mapping properties
+	 * 
+	 * @param propertiesNode
+	 * @param name
+	 * @param type
+	 * @param analyser
+	 * @param index
+	 * @param store
+	 */
+	private void addFieldMapping(ObjectNode propertiesNode, String name, String type, String analyser, Boolean index, Boolean store) {
+		ObjectNode field1Node = new ObjectMapper().createObjectNode();
+		field1Node.put("type", type);
+
+		// Optional: Set the analyzer if provided
+		if (analyser != null && !analyser.isEmpty()) {
+			field1Node.put("analyzer", analyser);
+		}
+
+		// Optional: Set the index option if provided
+		if (index != null) {
+			field1Node.put("index", index);
+		}
+
+		// Optional: Set the store option if provided
+		if (store != null) {
+			field1Node.put("store", store);
+		}
+
+		// Add this field's mapping to the properties node
+		propertiesNode.set(name, field1Node);
+	}
+
+	/**
+	 * Add a field to the mapping properties
+	 * 
+	 * @param propertiesNode
+	 * @param name
+	 * @param type
+	 * @param analyser
+	 * @param index
+	 */
+	private void addFieldMapping(ObjectNode propertiesNode, String name, String type, String analyser, Boolean index) {
+		addFieldMapping(propertiesNode, name, type, analyser, index, false);
+	}
+
+	/**
+	 * Add a field to the mapping properties
+	 * 
+	 * @param propertiesNode
+	 * @param name
+	 * @param type
+	 * @param index
+	 */
+	private void addFieldMapping(ObjectNode propertiesNode, String name, String type, Boolean index) {
+		addFieldMapping(propertiesNode, name, type, null, index, false);
+	}
+
+	/**
+	 * Create the analyser part
+	 * 
+	 * @param rootNode
+	 * @throws JsonProcessingException 
+	 * @throws JsonMappingException 
+	 */
+	private void configureAnalyser(ObjectNode rootNode) throws JsonMappingException, JsonProcessingException {
+		String analyserJson = """
+{
+  "index": {
+  	"refresh_interval": "30s"
+  },
   "analysis": {
     "analyzer": {
       "french": {
@@ -132,46 +216,12 @@ public class VerticalsRepositoryService {
     }
   }
 }
+				""";
 
-     * @param rootNode
-     */
-	private void configureAnalyser(ObjectNode rootNode) {
-		// "analysis": { "analyzer": { "french": { ... } } }
-		ObjectNode analysisNode = mapper.createObjectNode();
-		ObjectNode analyzerNode = mapper.createObjectNode();
-		ObjectNode frenchAnalyzerNode = mapper.createObjectNode();
+			JsonNode analysisNode = mapper.readTree(analyserJson);
 
-		// "french": { "type": "custom", "tokenizer": "standard", "char_filter": [...], "filter": [...] }
-		frenchAnalyzerNode.put("type", "custom");
-		frenchAnalyzerNode.put("tokenizer", "standard");
+			// TODO : Set analyser at the correct location
+			//			rootNode.set("analysis", analysisNode.get("analysis"));
 
-		// "char_filter": ["html_strip"]
-		ArrayNode charFilterArray = mapper.createArrayNode();
-		charFilterArray.add("html_strip");
-		frenchAnalyzerNode.set("char_filter", charFilterArray);
-
-		// "filter": ["lowercase", "asciifolding"]
-		ArrayNode filterArray = mapper.createArrayNode();
-		filterArray.add("lowercase");
-		filterArray.add("asciifolding");
-		frenchAnalyzerNode.set("filter", filterArray);
-
-		// Nest the frenchAnalyzerNode within analyzerNode
-		analyzerNode.set("french", frenchAnalyzerNode);
-
-		// Nest the analyzerNode within analysisNode
-		analysisNode.set("analyzer", analyzerNode);
-
-		// Finally, nest the analysisNode within rootNode
-		rootNode.set("analysis", analysisNode);
 	}
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
