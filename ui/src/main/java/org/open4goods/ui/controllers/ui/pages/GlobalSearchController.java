9dfbe6f782c0318218f0b82eb227d5979f3e6d1f
goulven <goulven@goulven-Ionico-IV-15>
2024-08-21 10:07:12 +0200

    Content : index review

--- ui/src/main/java/org/open4goods/ui/controllers/ui/pages/GlobalSearchController.java
+++ ui/src/main/java/org/open4goods/ui/controllers/ui/pages/GlobalSearchController.java
@@ -62,7 +62,7 @@ public class GlobalSearchController  implements SitemapExposedController{
 
 	@GetMapping({FR_PATH, DEFAULT_PATH})
 	@Cacheable(cacheNames = CacheConstants.ONE_HOUR_LOCAL_CACHE_NAME)
-	public ModelAndView searchGet(final HttpServletRequest request, @RequestParam String q) {
+	public ModelAndView searchGet(final HttpServletRequest request, @RequestParam(required = false, defaultValue = "") String q) {
 
 			ModelAndView model = uiService.defaultModelAndView("search", request);
 
