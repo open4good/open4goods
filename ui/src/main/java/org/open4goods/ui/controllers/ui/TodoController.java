96b905627fb7a99d242fadd4fc921adee856dc84
goulven <goulven@goulven-Ionico-IV-15>
2024-08-25 00:09:18 +0200

    Comment

--- ui/src/main/java/org/open4goods/ui/controllers/ui/TodoController.java
+++ ui/src/main/java/org/open4goods/ui/controllers/ui/TodoController.java
@@ -1,28 +1,19 @@
 package org.open4goods.ui.controllers.ui;
 
-import java.io.IOException;
-import java.util.Locale;
-
 import org.open4goods.ui.services.todo.TodoService;
-import org.open4goods.xwiki.model.FullPage;
-import org.open4goods.xwiki.services.XWikiHtmlService;
-import org.open4goods.xwiki.services.XwikiFacadeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 /**
- * This controller pages pageSize Xwiki content
+ * This controller renders the /todos page
  * 
- * @author gof TODO : Could put in the xwiki-starter
- *
  */
 
 @Controller
