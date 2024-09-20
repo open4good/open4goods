43d07a2742c4a5acda3f7d4133f28836451092ab
scezen <nootybear@gmail.com>
2024-09-20 11:33:27 +0200

    From conf + Tweaks

---
+++ ui/src/main/java/org/open4goods/ui/config/yml/AmazonConfig.java
@@ -0,0 +1,24 @@
+package org.open4goods.ui.config.yml;
+
+public class AmazonConfig {
+
+    private String affiliateTag;
+    private String amazonSearchLink;
+
+    public String getAffiliateTag() {
+        return affiliateTag;
+    }
+
+    public void setAffiliateTag(String affiliateTag) {
+        this.affiliateTag = affiliateTag;
+    }
+
+    public String getAmazonSearchLink() {
+        return amazonSearchLink;
+    }
+
+    public void setAmazonSearchLink(String amazonSearchLink) {
+        this.amazonSearchLink = amazonSearchLink;
+    }
+}
+
