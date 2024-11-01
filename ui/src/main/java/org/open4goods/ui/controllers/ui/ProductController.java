0b257c453e512618609b7ebe22c80a0cb4cbb2d2
goulven <goulven@goulven-Ionico-IV-15>
2024-11-01 23:31:01 +0100

    Adding log

--- ui/src/main/java/org/open4goods/ui/controllers/ui/ProductController.java
+++ ui/src/main/java/org/open4goods/ui/controllers/ui/ProductController.java
@@ -141,195 +141,201 @@ public class ProductController  {
 	private ModelAndView buildProductView(Long id, String vertical, final HttpServletRequest request,
 			HttpServletResponse response) {
 		
-		// Getting the product name
-		String path= URLEncoder.encode(request.getServletPath().substring(1), StandardCharsets.UTF_8);
-		
-		
-		// Retrieve the Product
-		Product data;
 		try {
-			data = productRepository.getById(id);
-
-		} catch (ResourceNotFoundException e) {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Produit " + request.getServletPath() + " introuvable !");
-		}
-
-		if (null == data) {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Produit " + request.getServletPath() + " introuvable !");
-		}
-
-
-		ModelAndView mv = null;
-
-
-		
-		
-		
-		mv = uiService.defaultModelAndView("product", request);
-
-		if (mv.getModel().get("user") != null) {
-			mv.addObject("raw", serialisationService.toJson(data, true));
-		}
-
-
-		mv.addObject("product", data);
-		
-		// Fetching better and best objects
-		if (null != data.ecoscore()) {
+			// Getting the product name
+			String path= URLEncoder.encode(request.getServletPath().substring(1), StandardCharsets.UTF_8);
 			
-			Long globalBestId = data.getRanking().getGlobalBest();
-			Long globalBetter = data.getRanking().getGlobalBetter();
 			
-			if (null != globalBestId && globalBestId.longValue() !=0) {
-				Product best = null;
-				try {
-					best = productRepository.getById(globalBestId);
-				} catch (ResourceNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+			// Retrieve the Product
+			Product data;
+			try {
+				data = productRepository.getById(id);
+
+			} catch (ResourceNotFoundException e) {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Produit " + request.getServletPath() + " introuvable !");
+			}
+
+			if (null == data) {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Produit " + request.getServletPath() + " introuvable !");
+			}
+
+
+			ModelAndView mv = null;
+
+
+			
+			
+			
+			mv = uiService.defaultModelAndView("product", request);
+
+			if (mv.getModel().get("user") != null) {
+				mv.addObject("raw", serialisationService.toJson(data, true));
+			}
+
+
+			mv.addObject("product", data);
+			
+			// Fetching better and best objects
+			if (null != data.ecoscore()) {
+				
+				Long globalBestId = data.getRanking().getGlobalBest();
+				Long globalBetter = data.getRanking().getGlobalBetter();
+				
+				if (null != globalBestId && globalBestId.longValue() !=0) {
+					Product best = null;
+					try {
+						best = productRepository.getById(globalBestId);
+					} catch (ResourceNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					mv.addObject("best", best);
 				}
-				mv.addObject("best", best);
-			}
-			
-			if (null != globalBetter && globalBetter.longValue() != 0) {
-				Product better = null;
-				try {
-					better = productRepository.getById(globalBetter);
-				} catch (ResourceNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				
+				if (null != globalBetter && globalBetter.longValue() != 0) {
+					Product better = null;
+					try {
+						better = productRepository.getById(globalBetter);
+					} catch (ResourceNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					mv.addObject("better", better);
 				}
-				mv.addObject("better", better);
+				
+				
 			}
 			
 			
-		}
-		
-		
-		// Adding the cover image
-		
-		String cover = "/icons/no-image.png";
-		if (data.getCoverImagePath() != null) {
-            cover =  data.getCoverImagePath();
-        } else {
-        	cover = data.externalCover();
-        }
-		mv.addObject("cover", cover);
-		
-		// Easiying accessess to pros and cons
-		
-		List<String> pros = null;
-		List<String> cons = null;
-		List<String> globalDescriptionParagraphs = null;
-		List<String> ecologicalDescriptionParagraphs = null;
-		
-		AiDescriptions aiDescriptions = data.getGenaiTexts().i18n(request);
-		if (null != aiDescriptions) {
+			// Adding the cover image
 			
-			AiDescription ps = aiDescriptions.getDescriptions().get("pros");
-			if (null != ps) {
-				pros = (Arrays.asList(ps.getContent().split("\n|<br/>|;")));
+			String cover = "/icons/no-image.png";
+			if (data.getCoverImagePath() != null) {
+			    cover =  data.getCoverImagePath();
+			} else {
+				cover = data.externalCover();
+			}
+			mv.addObject("cover", cover);
+			
+			// Easiying accessess to pros and cons
+			
+			List<String> pros = null;
+			List<String> cons = null;
+			List<String> globalDescriptionParagraphs = null;
+			List<String> ecologicalDescriptionParagraphs = null;
+			
+			AiDescriptions aiDescriptions = data.getGenaiTexts().i18n(request);
+			if (null != aiDescriptions) {
+				
+				AiDescription ps = aiDescriptions.getDescriptions().get("pros");
+				if (null != ps) {
+					pros = (Arrays.asList(ps.getContent().split("\n|<br/>|;")));
+				}
+				
+				AiDescription cs = data.getGenaiTexts().i18n(request).getDescriptions().get("cons");
+				if (null != cs) {
+					cons = (Arrays.asList(cs.getContent().split("\n|<br/>|;")));
+				}
+
+				AiDescription gd = data.getGenaiTexts().i18n(request).getDescriptions().get("global-description");
+				if (gd != null) {
+					globalDescriptionParagraphs = Arrays.asList(gd.getContent().split(";"));
+				}
+
+				AiDescription ed = data.getGenaiTexts().i18n(request).getDescriptions().get("ecological-description");
+				if (ed != null) {
+					ecologicalDescriptionParagraphs = Arrays.asList(ed.getContent().split(";"));
+				}
+			}
+
+			mv.addObject("pros", pros);
+			mv.addObject("cons", cons);
+			mv.addObject("globalDescriptionParagraphs", globalDescriptionParagraphs);
+			mv.addObject("ecologicalDescriptionParagraphs", ecologicalDescriptionParagraphs);
+			
+			// Building the pricetrend
+			
+			PriceTrend newTrends = PriceTrend.of(data.getPrice().getNewPricehistory());
+			
+			mv.addObject("newTrend", newTrends);
+			
+			
+			VerticalConfig verticalConfig = verticalConfigService.getVerticalForPath(vertical);
+			
+			List<AttributesFeatureGroups> features = icecatService.features(verticalConfig, mv.getModelMap().get("siteLanguage").toString(), data);
+			mv.addObject("features",features);
+			
+			
+			mv.addObject("verticalConfig", verticalConfig);
+
+			UiHelper uiHelper = new UiHelper(request, verticalConfig, data);
+			// Adding the UiHelper class
+			mv.addObject("helper", uiHelper);
+			
+			if (null == vertical) {
+
+				// Sending 301 id no match with product name
+				if (uiHelper.url() != null && !path.equals(uiHelper.url())) {
+					response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+					// TODO : I18n
+					mv = new ModelAndView("redirect:/" + uiHelper.url());
+					mv.setStatus(HttpStatus.MOVED_PERMANENTLY);
+					mv.addObject("product", data);
+					return mv;
+				}
+			} else {
+				if (null == data.getVertical()) {
+					// TODO : I18n path (fr)
+					mv = new ModelAndView(
+							"redirect:/" + uiHelper.url());
+					mv.setStatus(HttpStatus.MOVED_PERMANENTLY);
+					mv.addObject("product", data);
+					return mv;
+				} else	if (!vertical.equals(verticalConfigService.getPathForVerticalLanguage(mv.getModel().get("siteLanguage").toString(), verticalConfig))) {
+					String p = verticalConfigService.getPathForVerticalLanguage(mv.getModel().get("siteLanguage").toString(), verticalConfig);
+					mv = new ModelAndView("redirect:/"+ p+"/"+uiHelper.url());
+					mv.setStatus(HttpStatus.MOVED_PERMANENTLY);
+					mv.addObject("product", data);
+					return mv;
+				} else if (!path.equals(vertical+ "%2F" + URLEncoder.encode(uiHelper.url(), StandardCharsets.UTF_8))) {
+					mv = new ModelAndView("redirect:/"+ vertical+"/"+uiHelper.url());
+					mv.setStatus(HttpStatus.MOVED_PERMANENTLY);
+					mv.addObject("product", data);
+					return mv;
+				}
+			}
+
+
+			// Adding the affiliationTokens in all prices
+			for (AggregatedPrice price : data.getPrice().getOffers()) {
+				inferAffiliationToken(request, data, price);
+			}
+
+			
+			// Adding the affiliationTokens in min and max price
+			if (null != data.getPrice().getMinPrice()) {
+				inferAffiliationToken(request, data, data.getPrice().getMinPrice());
+			//		inferAffiliationToken(data, data.getPrice().getMaxPrice());
 			}
 			
-			AiDescription cs = data.getGenaiTexts().i18n(request).getDescriptions().get("cons");
-			if (null != cs) {
-				cons = (Arrays.asList(cs.getContent().split("\n|<br/>|;")));
+			
+			// Adding the diplay country
+			if (null != data.getGtinInfos().getCountry()) {
+				mv.addObject("originCountry", new ULocale("",data.getGtinInfos().getCountry()).getDisplayCountry( new ULocale(request.getLocale().toString())));
 			}
-	
-			AiDescription gd = data.getGenaiTexts().i18n(request).getDescriptions().get("global-description");
-			if (gd != null) {
-				globalDescriptionParagraphs = Arrays.asList(gd.getContent().split(";"));
-			}
-	
-			AiDescription ed = data.getGenaiTexts().i18n(request).getDescriptions().get("ecological-description");
-			if (ed != null) {
-				ecologicalDescriptionParagraphs = Arrays.asList(ed.getContent().split(";"));
-			}
+
+			
+			// Adding the brand informations
+			mv.addObject("hasBrandLogo", brandService.hasLogo(data.brand()));
+			
+			// Adding the images resource
+
+			return mv;
+		} catch (Exception e) {
+			LOGGER.error("Error while building view for {}",id,e);
+			
+			throw e;
 		}
-
-		mv.addObject("pros", pros);
-		mv.addObject("cons", cons);
-		mv.addObject("globalDescriptionParagraphs", globalDescriptionParagraphs);
-		mv.addObject("ecologicalDescriptionParagraphs", ecologicalDescriptionParagraphs);
-		
-		// Building the pricetrend
-		
-		PriceTrend newTrends = PriceTrend.of(data.getPrice().getNewPricehistory());
-		
-		mv.addObject("newTrend", newTrends);
-		
-		
-		VerticalConfig verticalConfig = verticalConfigService.getVerticalForPath(vertical);
-		
-		List<AttributesFeatureGroups> features = icecatService.features(verticalConfig, mv.getModelMap().get("siteLanguage").toString(), data);
-		mv.addObject("features",features);
-		
-		
-		mv.addObject("verticalConfig", verticalConfig);
-
-		UiHelper uiHelper = new UiHelper(request, verticalConfig, data);
-		// Adding the UiHelper class
-		mv.addObject("helper", uiHelper);
-		
-		if (null == vertical) {
-
-			// Sending 301 id no match with product name
-			if (uiHelper.url() != null && !path.equals(uiHelper.url())) {
-				response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
-				// TODO : I18n
-				mv = new ModelAndView("redirect:/" + uiHelper.url());
-				mv.setStatus(HttpStatus.MOVED_PERMANENTLY);
-				mv.addObject("product", data);
-				return mv;
-			}
-		} else {
-			if (null == data.getVertical()) {
-				// TODO : I18n path (fr)
-				mv = new ModelAndView(
-						"redirect:/" + uiHelper.url());
-				mv.setStatus(HttpStatus.MOVED_PERMANENTLY);
-				mv.addObject("product", data);
-				return mv;
-			} else	if (!vertical.equals(verticalConfigService.getPathForVerticalLanguage(mv.getModel().get("siteLanguage").toString(), verticalConfig))) {
-				String p = verticalConfigService.getPathForVerticalLanguage(mv.getModel().get("siteLanguage").toString(), verticalConfig);
-				mv = new ModelAndView("redirect:/"+ p+"/"+uiHelper.url());
-				mv.setStatus(HttpStatus.MOVED_PERMANENTLY);
-				mv.addObject("product", data);
-				return mv;
-			} else if (!path.equals(vertical+ "%2F" + URLEncoder.encode(uiHelper.url(), StandardCharsets.UTF_8))) {
-				mv = new ModelAndView("redirect:/"+ vertical+"/"+uiHelper.url());
-				mv.setStatus(HttpStatus.MOVED_PERMANENTLY);
-				mv.addObject("product", data);
-				return mv;
-			}
-		}
-
-
-		// Adding the affiliationTokens in all prices
-		for (AggregatedPrice price : data.getPrice().getOffers()) {
-			inferAffiliationToken(request, data, price);
-		}
-
-		
-		// Adding the affiliationTokens in min and max price
-		if (null != data.getPrice().getMinPrice()) {
-			inferAffiliationToken(request, data, data.getPrice().getMinPrice());
-		//		inferAffiliationToken(data, data.getPrice().getMaxPrice());
-		}
-		
-		
-		// Adding the diplay country
-		if (null != data.getGtinInfos().getCountry()) {
-			mv.addObject("originCountry", new ULocale("",data.getGtinInfos().getCountry()).getDisplayCountry( new ULocale(request.getLocale().toString())));
-		}
-
-		
-		// Adding the brand informations
-		mv.addObject("hasBrandLogo", brandService.hasLogo(data.brand()));
-		
-		// Adding the images resource
-
-		return mv;
 	}
 	
 	/**
