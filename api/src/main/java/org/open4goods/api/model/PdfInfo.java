265a89d034c42e1cee17314be0ad2f669d440261
scezen <nootybear@gmail.com>
2024-12-02 15:16:26 +0100

    PdfInfo POJO

---
+++ api/src/main/java/org/open4goods/api/model/PdfInfo.java
@@ -0,0 +1,106 @@
+package org.open4goods.api.model;
+
+import java.util.Calendar;
+
+public class PdfInfo {
+
+    private long size;
+    private String fileName;
+    private String metadataTitle;
+    private String extractedTitle;
+    private int numberOfPages;
+    private String author;
+    private String subject;
+    private String keywords;
+    private Calendar creationDate;
+    private Calendar modificationDate;
+    private String producer;
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getMetadataTitle() {
+        return metadataTitle;
+    }
+
+    public void setMetadataTitle(String metadataTitle) {
+        this.metadataTitle = metadataTitle;
+    }
+
+    public String getExtractedTitle() {
+        return extractedTitle;
+    }
+
+    public void setExtractedTitle(String extractedTitle) {
+        this.extractedTitle = extractedTitle;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public Calendar getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Calendar creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Calendar getModificationDate() {
+        return modificationDate;
+    }
+
+    public void setModificationDate(Calendar modificationDate) {
+        this.modificationDate = modificationDate;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+}
