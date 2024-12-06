8d8cfba8eb86217591a6e4b4cfc628302a116c08
goulven <goulven@goulven-Ionico-IV-15>
2024-12-06 14:30:43 +0100

    Dependencies update

--- ui/gulpfile.mjs
+++ ui/gulpfile.mjs
@@ -1,35 +1,22 @@
-/*
+import autoprefixer from 'gulp-autoprefixer';
+import browserSyncPkg from 'browser-sync';
+import cleanCss from 'gulp-clean-css';
+import { deleteAsync } from 'del';
+import htmlmin from 'gulp-htmlmin';
+import cssbeautify from 'gulp-cssbeautify';
+import gulp from 'gulp';
+import npmDist from 'gulp-npm-dist';
+import wait from 'gulp-wait';
+import sourcemaps from 'gulp-sourcemaps';
+import fileinclude from 'gulp-file-include';
 
-=========================================================
-* Pixel Pro Bootstrap 5 UI Kit
-=========================================================
+import gulpSass from 'gulp-sass';
+import * as dartSass from 'sass';
+const sass = gulpSass(dartSass);
 
-* Product Page: https://themesberg.com/product/ui-kit/pixel-pro-premium-bootstrap-5-ui-kit
-* Copyright 2021 Themesberg (https://www.themesberg.com)
-
-* Coded by https://themesberg.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. Contact us if you want to remove it.
-
-*/
-
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
-var cleanCss = require('gulp-clean-css');
-var del = require('del');
-const htmlmin = require('gulp-htmlmin');
-const cssbeautify = require('gulp-cssbeautify');
-var gulp = require('gulp');
-const npmDist = require('gulp-npm-dist');
-var sass = require('gulp-sass')(require('sass'));
-var wait = require('gulp-wait');
-var sourcemaps = require('gulp-sourcemaps');
-var fileinclude = require('gulp-file-include');
+const browserSync = browserSyncPkg.create();
 
 // Define paths
-
 const paths = {
     dist: {
         base: './dist/',
@@ -55,7 +42,7 @@ const paths = {
     }
 };
 
-// Compile SCSS
+// Tasks
 gulp.task('scss', function () {
     return gulp.src([paths.src.scss + '/custom/**/*.scss', paths.src.scss + '/pixel/**/*.scss', paths.src.scss + '/pixel.scss'])
         .pipe(wait(500))
@@ -69,7 +56,6 @@ gulp.task('scss', function () {
         .pipe(browserSync.stream());
 });
 
-// Beautify CSS
 gulp.task('beautify:css', function () {
     return gulp.src([
         paths.dev.css + '/pixel.css'
@@ -78,26 +64,22 @@ gulp.task('beautify:css', function () {
         .pipe(gulp.dest(paths.dev.css))
 });
 
-// Minify CSS
 gulp.task('minify:css', function () {
     return gulp.src([
         paths.dist.css + '/pixel.css'
     ])
-    .pipe(cleanCss())
-    .pipe(gulp.dest(paths.dist.css))
+        .pipe(cleanCss())
+        .pipe(gulp.dest(paths.dist.css))
 });
 
-
-// Clean
 gulp.task('clean:dist', function () {
-    return del([paths.dist.base]);
+    return deleteAsync([paths.dist.base]);
 });
 
 gulp.task('clean:dev', function () {
-    return del([paths.dev.base]);
+    return deleteAsync([paths.dev.base]);
 });
 
-// Compile and copy scss/css
 gulp.task('copy:dist:css', function () {
     return gulp.src([paths.src.scss + '/custom/**/*.scss', paths.src.scss + '/pixel/**/*.scss', paths.src.scss + '/pixel.scss'])
         .pipe(wait(500))
@@ -122,28 +104,18 @@ gulp.task('copy:dev:css', function () {
         .pipe(gulp.dest(paths.dev.css))
 });
 
-
 gulp.task('build:dev', gulp.series('copy:dev:css', 'beautify:css'));
 gulp.task('build:dist', gulp.series('clean:dist', 'copy:dist:css', 'minify:css'));
 
-// Watch for SCSS changes during development
 gulp.task('watch', function () {
-  
-    // Initialize BrowserSync to serve files from the root directory
-     browserSync.init({
-       proxy: "localhost:8082", // Use your Spring Boot local server URL
-       port: 3000
-     });
+    browserSync.init({
+        proxy: "localhost:8082",
+        port: 3000
+    });
 
-     // Watch SCSS changes
-        gulp.watch(paths.src.scss, gulp.series('copy:dev:css', 'beautify:css'));
-
-     // Watch for CSS changes and reload the browser
-      gulp.watch(paths.dev.css + '/**/*.css').on('change', browserSync.reload);
-      gulp.watch(paths.dev.templates + '/**/*.html').on('change', browserSync.reload);
-       
-    
-    
+    gulp.watch(paths.src.scss, gulp.series('copy:dev:css', 'beautify:css'));
+    gulp.watch(paths.dev.css + '/**/*.css').on('change', browserSync.reload);
+    gulp.watch(paths.dev.templates + '/**/*.html').on('change', browserSync.reload);
 });
-// Default
+
 gulp.task('default', gulp.series('build:dist'));
