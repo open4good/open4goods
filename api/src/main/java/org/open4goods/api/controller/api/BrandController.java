6ad37475dac301db99182442f8987b4d9392ce21
goulven <goulven@goulven-Ionico-IV-15>
2024-12-09 14:53:12 +0100

    Fixing stats restitution

--- api/src/main/java/org/open4goods/api/controller/api/BrandController.java
+++ api/src/main/java/org/open4goods/api/controller/api/BrandController.java
@@ -2,15 +2,11 @@
 
 package org.open4goods.api.controller.api;
 
-import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
-import org.open4goods.api.services.VerticalsGenerationService;
 import org.open4goods.commons.model.constants.RolesConstants;
 import org.open4goods.commons.services.BrandService;
-import org.open4goods.commons.services.VerticalsConfigService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,19 +32,15 @@ public class BrandController {
 
 	@GetMapping("/brands/stats/companies/missing")
 	@PreAuthorize("hasAuthority('" + RolesConstants.ROLE_XWIKI_ALL + "')")
-	public LinkedHashMap<String,String> statsMissingCompanies(HttpServletRequest request) {
+	public List<String> statsMissingCompanies(HttpServletRequest request) {
 	    // Fetch the stats map
 	    Map<String, Long> stats = brandService.getMissCounter();
 
 	    // Sort the map by value in descending order
 	   return  stats.entrySet().stream()
 	            .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
-	            .collect(Collectors.toMap(
-	                    Map.Entry::getKey,
-	                    entry -> "", // Replace the value with an empty string
-	                    (e1, e2) -> e1, // In case of duplicates, keep the first one
-	                    LinkedHashMap::new // Maintain the order
-	            ));
+	            .map(e->e.getKey())
+	            .toList();
 
 	}
 }
