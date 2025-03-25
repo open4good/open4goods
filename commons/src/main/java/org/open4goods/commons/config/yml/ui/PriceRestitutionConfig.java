9a339d1533ffb0e90e02820ff45cd345b4d10a15
goulven <goulven@goulven-Ionico-IV-15>
2025-03-25 09:07:30 +0100

    Ui

---
+++ commons/src/main/java/org/open4goods/commons/config/yml/ui/PriceRestitutionConfig.java
@@ -0,0 +1,23 @@
+package org.open4goods.commons.config.yml.ui;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.open4goods.model.Localisable;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class PriceRestitutionConfig {
+
+    private Localisable<String, List<Event>> events = new Localisable<>();
+
+    public Localisable<String, List<Event>> getEvents() {
+        return events;
+    }
+
+    public void setEvents(Localisable<String, List<Event>> events) {
+        this.events = events;
+    }
+
+    
+}
