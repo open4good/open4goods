a7d45a0a7cc06102b1d332d361b51391128a07de
goulven <goulven@goulven-Ionico-IV-15>
2025-06-23 23:28:09 +0200

    Merge branch 'main' of https://github.com/open4good/open4goods.git into main

--- admin/src/main/java/org/open4goods/sbadmin/SbAdminApplication.java
+++ admin/src/main/java/org/open4goods/sbadmin/SbAdminApplication.java
@@ -1,4 +1,4 @@
-package org.open4goods.admin;
+package org.open4goods.sbadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
