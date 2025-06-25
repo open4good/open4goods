b818ce87231af58a726705de3d9fd412aa021bf3
Goulven <71071559+GoulvenF@users.noreply.github.com>
2025-06-25 16:27:40 +0200

    feat(front-api): dynamic inclusion support

--- nudger-front-api/src/main/java/org/open4goods/nudgerfrontapi/config/AppConfig.java
+++ nudger-front-api/src/main/java/org/open4goods/nudgerfrontapi/config/AppConfig.java
@@ -5,6 +5,13 @@ import org.open4goods.services.remotefilecaching.config.RemoteFileCachingPropert
 import org.open4goods.services.remotefilecaching.service.RemoteFileCachingService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.context.WebApplicationContext;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 
 @Configuration
 public class AppConfig {
@@ -22,6 +29,27 @@ public class AppConfig {
     @Bean
     ProductRepository productRepository() {
         return new ProductRepository();
+    }
+
+    @Bean
+    @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
+    SimpleFilterProvider simpleFilterProvider() {
+        return new SimpleFilterProvider()
+                .setDefaultFilter(SimpleBeanPropertyFilter.serializeAll())
+                .setFailOnUnknownId(false);
+    }
+
+    @Bean
+    BeanPostProcessor filterProviderCustomizer(SimpleFilterProvider filters) {
+        return new BeanPostProcessor() {
+            @Override
+            public Object postProcessBeforeInitialization(Object bean, String name) {
+                if (bean instanceof Jackson2ObjectMapperBuilder builder) {
+                    builder.filters(filters);
+                }
+                return bean;
+            }
+        };
     }
 //
 //    @Bean
