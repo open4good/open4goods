6bd2fdfba74a8635a338ad0a35de5b283d05e1ec
Goulven <71071559+GoulvenF@users.noreply.github.com>
2025-06-25 02:09:18 +0200

    Wrap controller responses in ResponseEntity

--- nudger-front-api/src/main/java/org/open4goods/nudgerfrontapi/controller/AssociationsController.java
+++ nudger-front-api/src/main/java/org/open4goods/nudgerfrontapi/controller/AssociationsController.java
@@ -1,9 +1,12 @@
 package org.open4goods.nudgerfrontapi.controller;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.open4goods.nudgerfrontapi.dto.AssociationDto;
 import org.open4goods.nudgerfrontapi.service.AssociationsService;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +14,8 @@ import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 public class AssociationsController {
+
+    private static final long TTL_SECONDS = 300;
 
     private final AssociationsService associationsService;
 
@@ -20,7 +25,10 @@ public class AssociationsController {
 
     @GetMapping("/associations")
     @Operation(summary = "List associations")
-    public List<AssociationDto> associations() {
-        return associationsService.getAssociations();
+    public ResponseEntity<List<AssociationDto>> associations() {
+        List<AssociationDto> list = associationsService.getAssociations();
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(TTL_SECONDS, TimeUnit.SECONDS))
+                .body(list);
     }
 }
