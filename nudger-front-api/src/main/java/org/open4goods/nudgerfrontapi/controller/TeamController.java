2fbc3e04da9129870f462bf84ce55eb1d0a4f039
Goulven <71071559+GoulvenF@users.noreply.github.com>
2025-06-25 02:09:18 +0200

    Wrap controller responses in ResponseEntity

--- nudger-front-api/src/main/java/org/open4goods/nudgerfrontapi/controller/TeamController.java
+++ nudger-front-api/src/main/java/org/open4goods/nudgerfrontapi/controller/TeamController.java
@@ -1,9 +1,12 @@
 package org.open4goods.nudgerfrontapi.controller;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.open4goods.nudgerfrontapi.dto.TeamMemberDto;
 import org.open4goods.nudgerfrontapi.service.TeamService;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +14,8 @@ import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 public class TeamController {
+
+    private static final long TTL_SECONDS = 300;
 
     private final TeamService teamService;
 
@@ -20,7 +25,10 @@ public class TeamController {
 
     @GetMapping("/team")
     @Operation(summary = "List team members")
-    public List<TeamMemberDto> team() {
-        return teamService.getMembers();
+    public ResponseEntity<List<TeamMemberDto>> team() {
+        List<TeamMemberDto> members = teamService.getMembers();
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(TTL_SECONDS, TimeUnit.SECONDS))
+                .body(members);
     }
 }
