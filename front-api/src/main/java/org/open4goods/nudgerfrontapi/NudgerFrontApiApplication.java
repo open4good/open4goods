583eb5bcd38929451b057118953067b07d1bd48d
goulven <goulven@goulven-Ionico-IV-15>
2025-06-26 17:46:24 +0200

    Trying to fix HTTPS issue

--- front-api/src/main/java/org/open4goods/nudgerfrontapi/NudgerFrontApiApplication.java
+++ front-api/src/main/java/org/open4goods/nudgerfrontapi/NudgerFrontApiApplication.java
@@ -4,7 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @SpringBootApplication(scanBasePackages = "org.open4goods")
+@OpenAPIDefinition(
+		  servers = {
+		    @Server(url = "https://beta.front-api.nudger.fr", description = "Public beta HTTPS API")
+		  }
+		)
 @EnableCaching
 public class NudgerFrontApiApplication {
 
