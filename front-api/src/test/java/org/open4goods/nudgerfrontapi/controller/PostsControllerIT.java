4c0db9e20ba4c79a4a9da06ba82385b35c20e184
Goulven <71071559+GoulvenF@users.noreply.github.com>
2025-06-27 01:15:54 +0200

    front-api: add blog posts endpoint

---
+++ front-api/src/test/java/org/open4goods/nudgerfrontapi/controller/PostsControllerIT.java
@@ -0,0 +1,70 @@
+package org.open4goods.nudgerfrontapi.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.open4goods.nudgerfrontapi.controller.api.PostsController;
+import org.open4goods.services.blog.model.BlogPost;
+import org.open4goods.services.blog.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class PostsControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private PostsController controller;
+
+    @MockBean
+    private BlogService blogService;
+
+    @Test
+    void postsEndpointReturnsList() throws Exception {
+        BlogPost post = new BlogPost();
+        post.setTitle("Title");
+        post.setUrl("slug");
+        given(blogService.getPosts(any())).willReturn(List.of(post));
+
+        mockMvc.perform(get("/contents/posts").with(jwt()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].title").value("Title"));
+    }
+
+    @Test
+    void postEndpointReturns404WhenMissing() throws Exception {
+        given(blogService.getPostsByUrl()).willReturn(Map.of());
+
+        mockMvc.perform(get("/contents/posts/{slug}", "missing").with(jwt()))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void postEndpointReturnsData() throws Exception {
+        BlogPost post = new BlogPost();
+        post.setTitle("Title");
+        post.setUrl("slug");
+        post.setCreated(new Date(1L));
+        given(blogService.getPostsByUrl()).willReturn(Map.of("slug", post));
+
+        mockMvc.perform(get("/contents/posts/{slug}", "slug").with(jwt()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.url").value("slug"))
+                .andExpect(jsonPath("$.title").value("Title"));
+    }
+}
