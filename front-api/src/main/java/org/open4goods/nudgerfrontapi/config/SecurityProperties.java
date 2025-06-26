da4b69ea262a2efc399f84d16a29971679be0ca5
Goulven <71071559+GoulvenF@users.noreply.github.com>
2025-06-26 23:12:19 +0200

    feat(front-api): add CORS configuration

--- front-api/src/main/java/org/open4goods/nudgerfrontapi/config/SecurityProperties.java
+++ front-api/src/main/java/org/open4goods/nudgerfrontapi/config/SecurityProperties.java
@@ -2,6 +2,8 @@ package org.open4goods.nudgerfrontapi.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Configuration properties to enable or disable Spring Security for the
@@ -16,11 +18,24 @@ public class SecurityProperties {
      */
     private boolean enabled = true;
 
+    /**
+     * List of origins allowed for CORS requests.
+     */
+    private List<String> corsAllowedHosts = new ArrayList<>();
+
     public boolean isEnabled() {
         return enabled;
     }
 
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
+    }
+
+    public List<String> getCorsAllowedHosts() {
+        return corsAllowedHosts;
+    }
+
+    public void setCorsAllowedHosts(List<String> corsAllowedHosts) {
+        this.corsAllowedHosts = corsAllowedHosts;
     }
 }
