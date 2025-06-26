78b0d6656253c9427259b26e467f9f42878fae38
goulven <goulven@goulven-Ionico-IV-15>
2025-06-26 16:53:04 +0200

    Pageable

--- nudger-front-api/src/main/java/org/open4goods/nudgerfrontapi/dto/AbstractDTO.java
+++ nudger-front-api/src/main/java/org/open4goods/nudgerfrontapi/dto/AbstractDTO.java
@@ -1,6 +1,4 @@
-package org.open4goods.nudgerfrontapi.dto.product;
-
-import org.open4goods.nudgerfrontapi.dto.RequestMetadata;
+package org.open4goods.nudgerfrontapi.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
@@ -8,7 +6,7 @@ public abstract class AbstractDTO {
 
 
 
-    @Schema(description = "Timing metadata", nullable = true)
+    @Schema(description = "Request metadata", nullable = true)
     private RequestMetadata metadatas;
 
 	public RequestMetadata getMetadatas() {
--- nudger-front-api/src/main/java/org/open4goods/nudgerfrontapi/dto/product/ProductDto.java
+++ nudger-front-api/src/main/java/org/open4goods/nudgerfrontapi/dto/product/ProductDto.java
@@ -1,76 +1,96 @@
 package org.open4goods.nudgerfrontapi.dto.product;
 
-/**
- * Frontend view of a product. We make the choixe to wear the mapping logic in the DTO, adapted with our "composition" principe
- */
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.open4goods.nudgerfrontapi.dto.AbstractDTO;
+
+import com.google.common.base.Optional;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public class ProductDto extends AbstractDTO {
 
-		public enum ProductDtoComponent {
-			aiReview,
-			offers,
-			images
+	/**
+	 * Available facets on ProductsDto
+	 */
+	public enum ProductDtoComponent {
+		aiReview, offers, images
+	}
+
+	/**
+	 * Allowed sort values on Products. (must exactly match elastic mapping)
+	 */
+	public enum ProductDtoSortableFields {
+		price("price.minPrice.price"),
+		offersCount("offersCount");
+
+		private final String text;
+
+		ProductDtoSortableFields(String text) {
+			this.text = text;
 		}
 
-		public enum ProductDtoSortableFields {
-			price,
-			offersCount
-
+		public String getText() {
+			return text;
 		}
 
-
-
-
-        @Schema(description = "Product GTIN, it is the unique identifier", example = "7612345678901")
-        long gtin;
-
-        private ProductAiReviewDto aiReview;
-
-        private ProductOffersDto offers;
-
-        private ProductImagesDto images;
-
-
-
-		public long getGtin() {
-			return gtin;
+		@Override
+		public String toString() {
+			return text;
 		}
 
-		public void setGtin(long gtin) {
-			this.gtin = gtin;
+		/**
+		 * Optional: parse from the text value.
+		 */
+		private static final Map<String, ProductDtoSortableFields> LOOKUP = Arrays.stream(values()).collect(Collectors.toMap(ProductDtoSortableFields::getText, e -> e));
+
+		public static Optional<ProductDtoSortableFields> fromText(String text) {
+			return Optional.fromNullable(LOOKUP.get(text));
 		}
 
-		public ProductAiReviewDto getAiReview() {
-			return aiReview;
-		}
+	}
 
-		public void setAiReview(ProductAiReviewDto aiReview) {
-			this.aiReview = aiReview;
-		}
+	@Schema(description = "Product GTIN, it is the unique identifier", example = "7612345678901")
+	long gtin;
 
-		public ProductOffersDto getOffers() {
-			return offers;
-		}
+	private ProductAiReviewDto aiReview;
 
-		public void setOffers(ProductOffersDto offers) {
-			this.offers = offers;
-		}
+	private ProductOffersDto offers;
 
-		public ProductImagesDto getImages() {
-			return images;
-		}
+	private ProductImagesDto images;
 
-		public void setImages(ProductImagesDto images) {
-			this.images = images;
-		}
+	public long getGtin() {
+		return gtin;
+	}
 
+	public void setGtin(long gtin) {
+		this.gtin = gtin;
+	}
 
+	public ProductAiReviewDto getAiReview() {
+		return aiReview;
+	}
 
+	public void setAiReview(ProductAiReviewDto aiReview) {
+		this.aiReview = aiReview;
+	}
 
+	public ProductOffersDto getOffers() {
+		return offers;
+	}
 
+	public void setOffers(ProductOffersDto offers) {
+		this.offers = offers;
+	}
 
+	public ProductImagesDto getImages() {
+		return images;
+	}
 
-
+	public void setImages(ProductImagesDto images) {
+		this.images = images;
+	}
 
 }
