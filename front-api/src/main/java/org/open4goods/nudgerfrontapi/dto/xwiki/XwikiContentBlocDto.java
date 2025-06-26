02f832b8aebd09331924c79a8778c7edbc0a42e7
goulven <goulven@goulven-Ionico-IV-15>
2025-06-26 21:38:59 +0200

    Bloc contents draft

---
+++ front-api/src/main/java/org/open4goods/nudgerfrontapi/dto/xwiki/XwikiContentBlocDto.java
@@ -0,0 +1,16 @@
+package org.open4goods.nudgerfrontapi.dto.xwiki;
+
+import org.open4goods.nudgerfrontapi.dto.AbstractDTO;
+
+// TODO : Document (javadoc, spring doc, comments, ...)
+// TODO : convert to record
+
+public class XwikiContentBlocDto extends AbstractDTO{
+
+	private String blocId;
+	private String htmlContent;
+	private String editLink;
+
+
+
+}
