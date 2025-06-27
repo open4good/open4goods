b4874c635d55a05bb623232c2098366ae9ead719
Goulven <71071559+GoulvenF@users.noreply.github.com>
2025-06-27 22:56:42 +0200

    refactor(api): use records for search dto

--- api/src/main/java/org/open4goods/api/model/SearchResult.java
+++ api/src/main/java/org/open4goods/api/model/SearchResult.java
@@ -2,44 +2,14 @@ package org.open4goods.api.model;
 
 import org.open4goods.model.price.Price;
 
-public class SearchResult {
-	private String brand;
-	private String brandUid;
-	private Long dateIndexed;
-	private Price price;
-	private String url;
-	public String getBrand() {
-		return brand;
-	}
-	public void setBrand(final String brand) {
-		this.brand = brand;
-	}
-	public String getBrandUid() {
-		return brandUid;
-	}
-	public void setBrandUid(final String brandUid) {
-		this.brandUid = brandUid;
-	}
-
-	public Long getDateIndexed() {
-		return dateIndexed;
-	}
-	public void setDateIndexed(final Long dateIndexed) {
-		this.dateIndexed = dateIndexed;
-	}
-	public Price getPrice() {
-		return price;
-	}
-	public void setPrice(final Price price) {
-		this.price = price;
-	}
-	public String getUrl() {
-		return url;
-	}
-	public void setUrl(final String url) {
-		this.url = url;
-	}
-
-
-
+/**
+ * Search result item returned by various endpoints.
+ */
+public record SearchResult(
+        String brand,
+        String brandUid,
+        Long dateIndexed,
+        Price price,
+        String url) {
 }
+
