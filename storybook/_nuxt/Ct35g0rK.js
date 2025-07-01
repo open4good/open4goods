b328290348d1d1f819d0587eb0c15e8537acd650
GoulvenF <GoulvenF@users.noreply.github.com>
2025-07-01 10:40:16 +0000

    deploy: 7961bc70fbb4af1514166395877e4f4c55d17741

---
+++ storybook/_nuxt/Ct35g0rK.js
@@ -0,0 +1,21 @@
+import{_ as o}from"./cGvaELfe.js";import"./B0YrVYfe.js";import"./srNPEmaf.js";const u={component:o,title:"Blog/PostPreview"},e={render:a=>({components:{PostPreview:o},setup(){return{args:a}},template:'<PostPreview :post="args.post" />'}),args:{post:{title:"Hello",summary:"Summary",author:"Jane",category:["news"]}}};var t,r,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      PostPreview
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: '<PostPreview :post="args.post" />'
+  }),
+  args: {
+    post: {
+      title: 'Hello',
+      summary: 'Summary',
+      author: 'Jane',
+      category: ['news']
+    }
+  }
+}`,...(s=(r=e.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,u as default};
