939070475750fbb54176fddc2ba4dee2d7dab6c9
GoulvenF <GoulvenF@users.noreply.github.com>
2025-07-01 23:29:08 +0000

    deploy: fc868a2c932b0e33c2ba0b97b299c42ab39f39d1

---
+++ storybook/_nuxt/DxzLxOjF.js
@@ -0,0 +1,5 @@
+import{_ as t}from"./Ceq8xQ5N.js";import"./C2U_40Dc.js";const m={component:t,title:"Blog/Author"},e={args:{name:"Jane Doe"}};var a,r,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    name: 'Jane Doe'
+  }
+}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,m as default};
