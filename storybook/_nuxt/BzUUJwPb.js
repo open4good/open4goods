07223c561974f54eab50694acbc32a1f96c8fcdb
GoulvenF <GoulvenF@users.noreply.github.com>
2025-07-01 23:29:08 +0000

    deploy: d4c91d40d32919dee70d0a21a605d7dfdb218bc2

--- storybook/_nuxt/BzUUJwPb.js
+++
@@ -1,5 +0,0 @@
-import{_ as t}from"./srNPEmaf.js";import"./B0YrVYfe.js";const m={component:t,title:"Blog/Author"},e={args:{name:"Jane Doe"}};var a,r,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    name: 'Jane Doe'
-  }
-}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,m as default};
--- storybook/_nuxt/_zeWtZBg.js
+++ storybook/_nuxt/_zeWtZBg.js
@@ -1,4 +1,4 @@
-import{_ as o}from"./cGvaELfe.js";import"./B0YrVYfe.js";import"./srNPEmaf.js";const u={component:o,title:"Blog/PostPreview"},e={render:a=>({components:{PostPreview:o},setup(){return{args:a}},template:'<PostPreview :post="args.post" />'}),args:{post:{title:"Hello",summary:"Summary",author:"Jane",category:["news"]}}};var t,r,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+import{_ as o}from"./CAamefxZ.js";import"./C2U_40Dc.js";import"./Ceq8xQ5N.js";const u={component:o,title:"Blog/PostPreview"},e={render:a=>({components:{PostPreview:o},setup(){return{args:a}},template:'<PostPreview :post="args.post" />'}),args:{post:{title:"Hello",summary:"Summary",author:"Jane",category:["news"]}}};var t,r,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
   render: args => ({
     components: {
       PostPreview
