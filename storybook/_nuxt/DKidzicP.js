6446745ddd360de0c940a56b15f4622a60b2ed72
GoulvenF <GoulvenF@users.noreply.github.com>
2025-07-02 13:42:21 +0000

    deploy: 7bacc1288ef5e6ea711327cd85b450700e74e664

---
+++ storybook/_nuxt/DKidzicP.js
@@ -0,0 +1,8 @@
+import{_ as n}from"./BxdFYws3.js";import"./Bka4eoCP.js";const m={component:n,title:"Card"},e={render:()=>({components:{Card:n},template:"<Card>Example</Card>"})};var r,a,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      Card
+    },
+    template: '<Card>Example</Card>'
+  })
+}`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};const d=["Default"];export{e as Default,d as __namedExportsOrder,m as default};
