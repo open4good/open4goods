0d6d99cf04a6a65577cc1e59dc36afd777083429
Goulven <71071559+GoulvenF@users.noreply.github.com>
2025-07-15 13:48:29 +0200

    fix(frontend): resolve FetchError override

--- frontend/src/api/runtime.ts
+++ frontend/src/api/runtime.ts
@@ -266,7 +266,7 @@ export class ResponseError extends Error {
 
 export class FetchError extends Error {
     override name: "FetchError" = "FetchError";
-    constructor(public cause: Error, msg?: string) {
+    constructor(public override cause: Error, msg?: string) {
         super(msg);
     }
 }
