d9c444a74b81129247a402044818e70cc8cd8ad5
goulven <goulven@goulven-Ionico-IV-15>
2025-07-29 22:59:52 +0200

    enabling timeline

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,7 +2,13 @@
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: process.env.NODE_ENV !== 'production' },
+  devtools: {
+    enabled: process.env.NODE_ENV !== 'production',
+
+    timeline: {
+      enabled: true,
+    },
+  },
   typescript: {
     typeCheck: true,
     tsConfig: {
