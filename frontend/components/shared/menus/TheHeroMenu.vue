561622d024176ac24cb1ec5bf311b743411c4bc9
@Yann-mingant-hp <upsylon.dev@gmail.com>
2025-09-21 07:44:14 +0200

    feat(menu): update main menu

--- frontend/components/shared/menus/TheHeroMenu.vue
+++ frontend/components/shared/menus/TheHeroMenu.vue
@@ -1,14 +1,15 @@
 <template>
-    <menu>
-        <v-list class="d-flex justify-end">
-            <v-list-item
-            v-for="(item, index) in ['Home', 'About', 'Services', 'Contact']"
-            :key="index"
-            >
-            <v-list-item-title>{{ item }}</v-list-item-title>
-            </v-list-item>
-        </v-list>
-    </menu>
+  <menu>
+    <!-- TODO: translate -->
+    <v-list class="d-flex justify-end">
+      <v-list-item
+        v-for="(item, index) in ['Impact-score', 'Les produis', 'Blog', 'Contact']"
+        :key="index"
+      >
+        <v-list-item-title>{{ item }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </menu>
 </template>
 
 <script setup lang="ts">
