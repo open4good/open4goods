20e83ca3fbd8ca957efc37ab6e26756ddbeee0dc
goulven <goulven@goulven-Ionico-IV-15>
2025-09-24 10:53:31 +0200

    removed sticky footer

--- frontend/app/components/shared/footers/TheMainFooter.vue
+++ frontend/app/components/shared/footers/TheMainFooter.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <v-footer app height="auto" color="secondary" class="main-footer text-white pa-0">
+  <v-footer height="auto" color="secondary" class="main-footer text-white pa-0">
     <slot name="footer" />
   </v-footer>
 </template>
