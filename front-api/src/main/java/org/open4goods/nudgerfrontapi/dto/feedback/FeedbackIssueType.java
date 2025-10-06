64ffcb2ea73bb228163408a33eeeaa91490a8c81
Goulven <goulven@goulven-Ionico-IV-15>
2025-10-06 09:32:31 +0200

    Feedback fixes

--- front-api/src/main/java/org/open4goods/nudgerfrontapi/dto/feedback/FeedbackIssueType.java
+++ front-api/src/main/java/org/open4goods/nudgerfrontapi/dto/feedback/FeedbackIssueType.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 /**
  * Enumerates the supported categories of feedback that can be submitted.
  */
-@Schema(description = "Type of feedback entry.", allowableValues = {"IDEA", "BUG"})
+@Schema(description = "Type of feedback entry.")
 public enum FeedbackIssueType {
 
     /** A suggestion or product evolution idea. */
