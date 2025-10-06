a23923bfdbd40f3eb865806e98737b0a26fa013a
@Yann-mingant-hp <upsylon.dev@gmail.com>
2025-10-06 10:54:51 +0200

    feat(component): create slide component

---
+++ frontend/app/components/domains/home/sections/The-section-items-slide.vue
@@ -0,0 +1,25 @@
+<template>
+  <The-slide :items="items" />
+</template>
+
+<script setup>
+ 
+// Mock images
+const items = ref([
+  'https://picsum.photos/150/150?random=1',
+  'https://picsum.photos/150/150?random=2',
+  'https://picsum.photos/150/150?random=3',
+  'https://picsum.photos/150/150?random=1',
+  'https://picsum.photos/150/150?random=2',
+  'https://picsum.photos/150/150?random=3',
+  'https://picsum.photos/150/150?random=1',
+  'https://picsum.photos/150/150?random=2',
+  'https://picsum.photos/150/150?random=3',
+  'https://picsum.photos/150/150?random=1',
+  'https://picsum.photos/150/150?random=2',
+  'https://picsum.photos/150/150?random=3',
+  'https://picsum.photos/150/150?random=1',
+  'https://picsum.photos/150/150?random=2',
+  'https://picsum.photos/150/150?random=3',
+])
+</script>
---
+++ frontend/app/components/shared/slides/The-slide.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-slide-group
+    show-arrows
+    :model-value="selectedImage"
+  >
+    <v-slide-group-item
+      v-for="(item, index) in items"
+      :key="index"
+      v-slot="{ isSelected, toggle }"
+    >
+      <v-card
+        :color="isSelected ? 'primary' : 'grey-lighten-1'"
+        class="ma-2"
+        height="80"
+        width="80"
+        @click="toggle"
+      >
+        <v-img
+          :src="item"
+          height="80"
+          cover
+        ></v-img>
+      </v-card>
+    </v-slide-group-item>
+  </v-slide-group>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const selectedImage = ref(null)
+
+const props = defineProps({
+  // Define any props if needed
+  items: Array,
+}) 
+</script>
