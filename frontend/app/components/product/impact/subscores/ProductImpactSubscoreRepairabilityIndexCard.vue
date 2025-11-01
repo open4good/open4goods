2692624a33d7ac37dfa444d32927b0c6e5133226
Goulven <71071559+GoulvenF@users.noreply.github.com>
2025-11-01 11:13:09 +0100

    feat(frontend): add specialized impact subscore cards

---
+++ frontend/app/components/product/impact/subscores/ProductImpactSubscoreRepairabilityIndexCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <ProductImpactSubscoreGenericCard v-bind="props" />
+</template>
+
+<script setup lang="ts">
+import ProductImpactSubscoreGenericCard from '../ProductImpactSubscoreGenericCard.vue'
+import type { ScoreView } from '../impact-types'
+
+const props = defineProps<{
+  score: ScoreView
+  productName: string
+}>()
+</script>
