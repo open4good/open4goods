a41b4166944873963d753a95b0af97125e138e78
Goulven <goulven@goulven-Ionico-IV-15>
2025-11-05 09:07:14 +0100

    index yann

---
+++ frontend/app/pages/index-yann.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const drawerStore = useState("mobileDrawer", () => false);
+
+const toggleDrawer = () => {
+  drawerStore.value = !drawerStore.value;
+};
+</script>
+
+<template>
+  <div class="full-height">
+    <Hero-section @toggle-drawer="toggleDrawer" />
+
+    <The-section-items-slide />
+
+    <Bloc3Hz />
+
+  </div>
+</template>
+
+<style lang="sass" scoped></style>
