1e5ac2699c35b1b49811d4142296aa41bb7517b4
Goulven <goulven@goulven-Ionico-IV-15>
2025-11-19 09:14:11 +0100

    Disabling health check

--- ui/src/main/java/org/open4goods/ui/services/todo/TodoService.java
+++ ui/src/main/java/org/open4goods/ui/services/todo/TodoService.java
@@ -31,7 +31,8 @@ import jakarta.annotation.PostConstruct;
  * A service quick and dirty, that allows parsing of T O D O's (maven taglist.xml)
  * @author goulven
  */
-public class TodoService implements HealthIndicator{
+public class TodoService //implements HealthIndicator
+{
 
 	private static final Logger logger = LoggerFactory.getLogger(TodoService.class);
 
@@ -42,14 +43,14 @@ public class TodoService implements HealthIndicator{
 		this.tagListPath = tagList;
 	}
 
-	private String tagListPath;	// A TOUDOU markup, in the form TOUDO(P1,design) : make a new feature 
+	private String tagListPath;	// A TOUDOU markup, in the form TOUDO(P1,design) : make a new feature
 	// the type, the priority, and the estimation can be swapped)
-	
+
 
 
 
 	/**
-	 * Download the taglist.xml file  
+	 * Download the taglist.xml file
 	 * @throws XPathExpressionException
 	 * @throws IOException
 	 * @throws Exception
@@ -60,7 +61,7 @@ public class TodoService implements HealthIndicator{
 		try {
 			f =  File.createTempFile("todo", "todo");
 			FileUtils.copyURLToFile(new URL( this.tagListPath), f);
-			this.todos = loadTodos(f);		
+			this.todos = loadTodos(f);
 		} catch (Exception e) {
 			logger.error("Error while loading taglist file at {} : {} ",tagListPath, e.getMessage());
 		} finally {
@@ -81,7 +82,7 @@ public class TodoService implements HealthIndicator{
 	public List<Todo> loadTodos(File taglistFile) throws Exception, IOException, XPathExpressionException {
 		Document doc = DocumentHelper.cleanAndGetDocument(FileUtils.readFileToString(taglistFile, StandardCharsets.UTF_8));
 
-		
+
 		// Getting todos from taglist.txt
 		List<Node> todoNodes = XpathHelper.xpathMultipleEval(doc, "//file");
 
@@ -90,7 +91,7 @@ public class TodoService implements HealthIndicator{
 		for (int i = 0; i < todoNodes.size(); i++) {
 
 			String fileName = todoNodes.get(i).getAttributes().getNamedItem("name").getTextContent();
-			
+
 			List<String> lineNumbers = XpathHelper.xpathMultipleEvalString(todoNodes.get(i), "./comments//lineNumber");
 			List<String> comments = XpathHelper.xpathMultipleEvalString(todoNodes.get(i), "./comments//comment/comment");
 
@@ -108,9 +109,9 @@ public class TodoService implements HealthIndicator{
 	 * @return
 	 */
 	private String detectComponent(String fileName) {
-		
+
 		String component = "";
-		
+
 		if (fileName.startsWith("org.open4goods.ui")) {
 			component = "ui";
 		} else if (fileName.startsWith("org.open4goods.model")){
@@ -124,12 +125,12 @@ public class TodoService implements HealthIndicator{
 		} else {
 			logger.warn("Cannot get TODO component for {}",fileName);
 		}
-		
+
 		return component;
 	}
 
 	/**
-	 * 
+	 *
 	 * @return the count of items by component
 	 */
 	public Map<String, Integer> byComponents() {
@@ -139,9 +140,9 @@ public class TodoService implements HealthIndicator{
 	                    Collectors.summingInt(e -> 1) // Count each item
 	                ));
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @return the count of items by priority
 	 */
 	public Map<String, Integer> byPriority() {
@@ -151,21 +152,21 @@ public class TodoService implements HealthIndicator{
 	                    Collectors.summingInt(e -> 1) // Count each item
 	                ));
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @return the count of items by category
 	 */
 	public Map<String, Integer> byCategory() {
 		   return todos.stream()
-				   	
+
 	                .collect(Collectors.groupingBy(
 	                    Todo::getCategoryOrUndefined, // Group by the component
 	                    Collectors.summingInt(e -> 1) // Count each item
 	                ));
 	}
-	
-	
+
+
 	public List<Todo> getTodos() {
 		return todos;
 	}
@@ -174,26 +175,26 @@ public class TodoService implements HealthIndicator{
 		this.todos = todos;
 	}
 
-	/**
-	 * Custom healthcheck, simply goes to DOWN if 0 T O DOs, that could never happens.
-	 */
-	@Override
-	public Health health() {
-		
-		Builder health;
-		
-		int eCount = todos.size();
-		
-		if (0 == eCount ) {
-			health =  Health.down();
-		} else {
-			health =  Health.up();
-		}
+//	/**
+//	 * Custom healthcheck, simply goes to DOWN if 0 T O DOs, that could never happens.
+//	 */
+//	@Override
+//	public Health health() {
+//
+//		Builder health;
+//
+//		int eCount = todos.size();
+//
+//		if (0 == eCount ) {
+//			health =  Health.down();
+//		} else {
+//			health =  Health.up();
+//		}
+//
+//		return health.withDetail("todos_count", eCount).build();
+//	}
+//
+//
+//
 
-		return health.withDetail("todos_count", eCount).build();
-	}
-
-
-	
-	
 }
