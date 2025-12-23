fe55ba9f62c038f24816a02b230705ca9dda40ea
Goulven <71071559+GoulvenF@users.noreply.github.com>
2025-12-23 23:14:47 +0100

    Add semantic vector search pipeline

---
+++ front-api/src/main/java/org/open4goods/nudgerfrontapi/config/TextEmbeddingConfig.java
@@ -0,0 +1,17 @@
+package org.open4goods.nudgerfrontapi.config;
+
+import org.open4goods.commons.services.TextEmbeddingService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TextEmbeddingConfig
+{
+    @Bean
+    @ConditionalOnMissingBean(TextEmbeddingService.class)
+    TextEmbeddingService textEmbeddingService()
+    {
+        return text -> null;
+    }
+}
