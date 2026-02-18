f54108dcef885e10e4df1effb16a8fb4eaba898b
Goulven <goulven@goulven-Ionico-IV-15>
2026-02-18 02:30:36 +0100

    index

--- model/src/main/java/org/open4goods/model/product/Product.java
+++ model/src/main/java/org/open4goods/model/product/Product.java
@@ -55,7 +55,7 @@ public class Product implements Standardisable {
 	/**
 	 * Name of the elastic index
 	 */
-	public static final String DEFAULT_REPO = "products-x";
+	public static final String DEFAULT_REPO = "products-moustik";
 
 	/**
 	 * Name of the ecoscore
